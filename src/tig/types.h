db5286a619aec3c690002e71619decc3dc833a9f
Alexander Batalov <alex.batalov@gmail.com>
2024-02-29 21:48:45 +0300

    Refactor memory

--- src/tig/types.h
+++ src/tig/types.h
@@ -1,22 +1,14 @@
 #ifndef ARCANUM_TIG_TYPES_H_
 #define ARCANUM_TIG_TYPES_H_
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include <windows.h>
 
-#define TIG_CONTEXT_FLAG_FPS 0x0001
-#define TIG_CONTEXT_FLAG_DOUBLE_BUFFER 0x2
-#define TIG_CONTEXT_FLAG_0x04 0x04
-#define TIG_CONTEXT_FLAG_0x10 0x10
-#define TIG_CONTEXT_FLAG_0x20 0x20
-#define TIG_CONTEXT_FLAG_0x40 0x40
-#define TIG_CONTEXT_FLAG_3D_SOFTWARE 0x200
-#define TIG_CONTEXT_FLAG_3D_HARDWARE 0x400
-#define TIG_CONTEXT_FLAG_3D_REF_DEVICE 0x800
-#define TIG_CONTEXT_FLAG_NO_SOUND 0x2000
-
-#define TIG_CONTEXT_FLAG_ANY_3D (TIG_CONTEXT_FLAG_3D_SOFTWARE | TIG_CONTEXT_FLAG_3D_HARDWARE | TIG_CONTEXT_FLAG_3D_REF_DEVICE)
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define TIG_OK 0
 #define TIG_NOT_INITIALIZED 1
@@ -32,10 +24,26 @@
 #define TIG_ERR_14 14
 #define TIG_ERR_16 16
 
+#define TIG_CONTEXT_FPS 0x0001
+#define TIG_CONTEXT_DOUBLE_BUFFER 0x0002
+#define TIG_CONTEXT_0x0004 0x0004
+#define TIG_CONTEXT_0x0008 0x0008
+#define TIG_CONTEXT_0x0010 0x0010
+#define TIG_CONTEXT_0x0020 0x0020
+#define TIG_CONTEXT_0x0040 0x0040
+#define TIG_CONTEXT_0x0080 0x0080
+#define TIG_CONTEXT_0x0100 0x0100
+#define TIG_CONTEXT_3D_SOFTWARE 0x0200
+#define TIG_CONTEXT_3D_HARDWARE 0x0400
+#define TIG_CONTEXT_3D_REF_DEVICE 0x0800
+#define TIG_CONTEXT_0x1000 0x1000
+#define TIG_CONTEXT_NO_SOUND 0x2000
+
+#define TIG_CONTEXT_ANY_3D (TIG_CONTEXT_3D_SOFTWARE | TIG_CONTEXT_3D_HARDWARE | TIG_CONTEXT_3D_REF_DEVICE)
+
 typedef int(TigArtFilePathResolver)(unsigned int art_id, char* path);
 typedef void(TigFilePathResolver)(int a1, char* path);
 typedef int(TigContextF20)(unsigned int art_id);
-typedef LRESULT(TigContextWindowProc)(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 typedef bool(TigContextMessageHandler)(LPMSG msg);
 
 typedef struct TigContext {
@@ -48,22 +56,19 @@ typedef struct TigContext {
     /* 0018 */ HINSTANCE instance;
     /* 001C */ TigArtFilePathResolver* art_file_path_resolver;
     /* 0020 */ TigContextF20* field_20;
-    /* 0024 */ TigContextWindowProc* default_window_proc;
+    /* 0024 */ WNDPROC default_window_proc;
     /* 0028 */ TigContextMessageHandler* message_handler;
     /* 002C */ TigFilePathResolver* sound_file_path_resolver;
     /* 0030 */ int field_30;
     /* 0034 */ unsigned int texture_width;
     /* 0038 */ unsigned int texture_height;
-    /* 003C */ int field_3C;
-};
+    /* 003C */ const char* window_name;
+} TigContext;
 
 static_assert(sizeof(TigContext) == 0x40, "wrong size");
 
-typedef struct TigRect {
-    int x;
-    int y;
-    int width;
-    int height;
-};
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* ARCANUM_TIG_TYPES_H_ */
