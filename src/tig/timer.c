ffc1bc887ea65cdafbe7ef1027ce21fb3a2890a5
Alexander Batalov <alex.batalov@gmail.com>
2024-03-11 20:48:47 +0300

    Refactor timer

---
+++ src/tig/timer.c
@@ -0,0 +1,51 @@
+#include "tig/timer.h"
+
+#include <limits.h>
+
+// clang-format off
+#include <windows.h>
+#include <timeapi.h>
+// clang-format on
+
+#define RESOLUTION 1
+
+// 0x52DF80
+int tig_timer_init(TigContext* ctx)
+{
+    (void)ctx;
+
+    if (timeBeginPeriod(RESOLUTION) != TIMERR_NOERROR) {
+        return 16;
+    }
+
+    return 0;
+}
+
+// 0x52DF90
+void tig_timer_exit()
+{
+    timeEndPeriod(RESOLUTION);
+}
+
+// 0x52DFA0
+int tig_timer_start(timer_t* start)
+{
+    *start = timeGetTime();
+    return 0;
+}
+
+// 0x52DFB0
+duration_t tig_timer_elapsed(timer_t start)
+{
+    return timeGetTime() - start;
+}
+
+// 0x52DFC0
+duration_t tig_timer_between(timer_t start, timer_t end)
+{
+    unsigned int diff = end - start;
+    if (diff > INT_MAX) {
+        diff = INT_MAX;
+    }
+    return diff;
+}
--- src/tig/timer.h
+++ src/tig/timer.h
@@ -3,10 +3,36 @@
 
 #include "tig/types.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// Represents time in milliseconds.
+///
+/// NOTE: On Windows this value represents number of milliseconds since the
+/// system was started. On other platforms it might be something else (relative
+/// to start of the game for example). In general the game does not rely on it's
+/// meaning. Instead it is considered as an time point used to calculate
+/// duration between two events.
+typedef unsigned int timer_t;
+
+/// Represents duration between two `timer_t` objects (in milliseconds).
+///
+/// NOTE: There is no consensus about signness of this type. The majority of
+/// call sites use signed compares, however there are several places with
+/// unsigned checks. One of the arguments for it to be signed is that
+/// `tig_timer_between` caps duration at 0x7FFFFFFF (that is max 32 bit signed
+/// int).
+typedef int duration_t;
+
 int tig_timer_init(TigContext* ctx);
 void tig_timer_exit();
-int tig_timer_start(unsigned int* start);
-unsigned int tig_timer_elapsed(unsigned int start);
-int tig_timer_between(unsigned int start, unsigned int end);
+int tig_timer_start(timer_t* start);
+duration_t tig_timer_elapsed(timer_t start);
+duration_t tig_timer_between(timer_t start, timer_t end);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* ARCANUM_TIG_TIMER_H_ */
