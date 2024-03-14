f1172147a540721c2d43e959952c0479e9ebcfe9
Alexander Batalov <alex.batalov@gmail.com>
2024-03-14 21:25:55 +0300

    Refactor kb

--- src/tig/kb.h
+++ src/tig/kb.h
@@ -3,10 +3,20 @@
 
 #include "tig/types.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int dword_62B1A8[16];
+
 int tig_kb_init(TigContext* ctx);
 void tig_kb_exit();
 bool tig_kb_is_key_pressed(int key);
 bool tig_kb_get_modifier(int key);
 void tig_kb_ping();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* ARCANUM_TIG_KB_H_ */
