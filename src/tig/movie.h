4872814b29dbe9092eadc0e0d927479a57bb0e17
Alexander Batalov <alex.batalov@gmail.com>
2024-03-16 22:29:43 +0300

    Refactor movie

--- src/tig/movie.h
+++ src/tig/movie.h
@@ -1,18 +1,35 @@
 #ifndef ARCANUM_TIG_MOVIE_H_
 #define ARCANUM_TIG_MOVIE_H_
 
+#include <stdbool.h>
+
 #include "tig/types.h"
 
-typedef enum MovieFlags {
-    MOVIE_FLAG_0x01 = 0x01,
-    MOVIE_FADE_IN = 0x02,
-    MOVIE_FADE_OUT = 0x04,
-    MOVIE_FLAG_0x08 = 0x08,
-};
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+typedef enum TigMovieFlags {
+    TIG_MOVIE_IGNORE_KEYBOARD = 0x01,
+    TIG_MOVIE_FADE_IN = 0x02,
+    TIG_MOVIE_FADE_OUT = 0x04,
+    TIG_MOVIE_BLACK_OUT = 0x08,
+} TigMovieFlags;
+
+/// Initializes movie system.
 int tig_movie_init(TigContext* ctx);
+
+/// Shutdowns movie system.
 void tig_movie_exit();
-int tig_movie_play(const char* path, unsigned int flags, int sound_track);
-BINK* tig_movie_get_bink();
+
+/// Play given movie file.
+int tig_movie_play(const char* path, unsigned int movie_flags, int sound_track);
+
+/// Returns `true` if movie is currently being played.
+bool tig_movie_is_playing();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* ARCANUM_TIG_MOVIE_H_ */
