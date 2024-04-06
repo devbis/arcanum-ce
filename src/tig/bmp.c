4983e06174f5c6d326829bb46783c4ff9f480d22
Alexander Batalov <alex.batalov@gmail.com>
2024-04-06 18:43:04 +0300

    Add tig_bmp_destroy

--- src/tig/bmp.c
+++ src/tig/bmp.c
@@ -1,5 +1,17 @@
 #include "tig/bmp.h"
 
+#include "tig/memory.h"
+
+// 0x5360D0
+int tig_bmp_destroy(TigBmp* bmp)
+{
+    if (bmp->pixels != NULL) {
+        FREE(bmp->pixels);
+    }
+
+    return TIG_OK;
+}
+
 // 0x5368B0
 int tig_bmp_copy_to_video_buffer(TigBmp* bmp, const TigRect* src_rect, TigVideoBuffer* video_buffer, const TigRect* dst_rect)
 {
