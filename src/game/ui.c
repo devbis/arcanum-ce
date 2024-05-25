964b584342ed82c2122a694f6c20529ce1e4749a
Alexander Batalov <alex.batalov@gmail.com>
2024-05-26 00:38:55 +0300

    Add ui

---
+++ src/game/ui.c
@@ -0,0 +1,750 @@
+#include "game/ui.h"
+
+#include <tig/tig.h>
+
+// 0x5E8658
+static UiCallbacks ui_callbacks;
+
+// 0x5E87C8
+static TigIdxTable stru_5E87C8;
+
+// 0x45FD40
+void ui_init(UiCallbacks* callbacks)
+{
+    if (callbacks != NULL) {
+        ui_callbacks = *callbacks;
+    }
+
+    tig_idxtable_init(&stru_5E87C8, 24);
+}
+
+// 0x460150
+void ui_exit()
+{
+    tig_idxtable_exit(&stru_5E87C8);
+}
+
+// 0x460160
+void ui_timeevent_process()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4601C0
+void sub_4601C0()
+{
+    if (ui_callbacks.field_0 != NULL) {
+        ui_callbacks.field_0();
+    }
+}
+
+// 0x4601D0
+void sub_4601D0(int a1, int a2)
+{
+    if (ui_callbacks.field_4 != NULL) {
+        ui_callbacks.field_4(a1, a2);
+    }
+}
+
+// 0x4601F0
+void sub_4601F0(int a1, int a2)
+{
+    if (ui_callbacks.field_8 != NULL) {
+        ui_callbacks.field_8(a1, a2);
+    }
+}
+
+// 0x460240
+void sub_460240(long long a1)
+{
+    if (ui_callbacks.field_10 != NULL) {
+        ui_callbacks.field_10(a1);
+    }
+}
+
+// 0x460260
+void sub_460260(int a1, int a2)
+{
+    if (ui_callbacks.field_14 != NULL) {
+        ui_callbacks.field_14(a1, a2);
+    }
+}
+
+// 0x460280
+void sub_460280(int a1, int a2)
+{
+    if (ui_callbacks.field_18 != NULL) {
+        ui_callbacks.field_18(a1, a2);
+    }
+}
+
+// 0x4602A0
+void sub_4602A0(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_1C != NULL) {
+        ui_callbacks.field_1C(a1, a2, a3, a4);
+    }
+}
+
+// 0x4602D0
+void sub_4602D0(long long a1, long long a2)
+{
+    if (ui_callbacks.field_20 != NULL) {
+        ui_callbacks.field_20(a1, a2);
+    }
+}
+
+// 0x460300
+void sub_460300(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_24 != NULL) {
+        ui_callbacks.field_24(a1, a2, a3, a4);
+    }
+}
+
+// 0x460330
+void sub_460330(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_28 != NULL) {
+        ui_callbacks.field_28(a1, a2, a3, a4);
+    }
+}
+
+// 0x460360
+void sub_460360(int a1, int a2, int a3)
+{
+    if (ui_callbacks.field_2C != NULL) {
+        ui_callbacks.field_2C(a1, a2, a3);
+    }
+}
+
+// 0x460380
+void sub_460380(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_30 != NULL) {
+        ui_callbacks.field_30(a1, a2, a3, a4);
+    }
+}
+
+// 0x4603E0
+void sub_4603E0(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_38 != NULL) {
+        ui_callbacks.field_38(a1, a2, a3, a4);
+    }
+}
+
+// 0x460410
+void sub_460410(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_3C != NULL) {
+        ui_callbacks.field_3C(a1, a2, a3, a4);
+    }
+}
+
+// 0x460440
+void sub_460440(int a1, int a2)
+{
+    if (ui_callbacks.field_40 != NULL) {
+        ui_callbacks.field_40(a1, a2);
+    }
+}
+
+// 0x460460
+int sub_460460(int a1, int a2)
+{
+    if (ui_callbacks.field_44 != NULL) {
+        return ui_callbacks.field_44(a1, a2);
+    } else {
+        return 0;
+    }
+}
+
+// 0x4604A0
+int sub_4604A0(int a1)
+{
+    if (ui_callbacks.field_4C != NULL) {
+        return ui_callbacks.field_4C(a1);
+    } else {
+        return 0;
+    }
+}
+
+// 0x4604C0
+void sub_4604C0(int a1)
+{
+    if (ui_callbacks.field_50 != NULL) {
+        ui_callbacks.field_50(a1);
+    }
+}
+
+// 0x4604E0
+void sub_4604E0()
+{
+    if (ui_callbacks.field_54 != NULL) {
+        ui_callbacks.field_54();
+    }
+}
+
+// 0x4604F0
+void sub_4604F0(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_58 != NULL) {
+        ui_callbacks.field_58(a1, a2, a3, a4);
+    }
+}
+
+// 0x460520
+void sub_460520()
+{
+    if (ui_callbacks.field_5C != NULL) {
+        ui_callbacks.field_5C();
+    }
+}
+
+// 0x460530
+void sub_460530()
+{
+    if (ui_callbacks.field_60 != NULL) {
+        ui_callbacks.field_60();
+    }
+}
+
+// 0x460540
+void sub_460540(int a1)
+{
+    if (ui_callbacks.field_64 != NULL) {
+        ui_callbacks.field_64(a1);
+    }
+}
+
+// 0x460560
+void sub_460560(int a1, int a2, int a3, int a4, int a5)
+{
+    if (ui_callbacks.field_68 != NULL) {
+        ui_callbacks.field_68(a1, a2, a3, a4, a5);
+    }
+}
+
+// 0x460590
+int sub_460590(int a1, int a2, int a3)
+{
+    if (ui_callbacks.field_6C != NULL) {
+        return ui_callbacks.field_6C(a1, a2, a3);
+    } else {
+        return 0;
+    }
+}
+
+// 0x4605C0
+void sub_4605C0()
+{
+    if (ui_callbacks.field_70 != NULL) {
+        ui_callbacks.field_70();
+    }
+}
+
+// 0x4605D0
+void sub_4605D0()
+{
+    if (ui_callbacks.field_74 != NULL) {
+        ui_callbacks.field_74();
+    }
+}
+
+// 0x4605E0
+void sub_4605E0(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_78 != NULL) {
+        ui_callbacks.field_78(a1, a2, a3, a4);
+    }
+}
+
+// 0x460610
+void sub_460610(int a1)
+{
+    if (ui_callbacks.field_7C != NULL) {
+        ui_callbacks.field_7C(a1);
+    }
+}
+
+// 0x460630
+void sub_460630(int a1)
+{
+    if (ui_callbacks.field_80 != NULL) {
+        ui_callbacks.field_80(a1);
+    }
+}
+
+// 0x460650
+void sub_460650()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4606C0
+void sub_4606C0(int a1)
+{
+    if (ui_callbacks.field_84 != NULL) {
+        ui_callbacks.field_84(a1);
+    }
+}
+
+// 0x4606E0
+void sub_4606E0()
+{
+    if (ui_callbacks.field_88 != NULL) {
+        ui_callbacks.field_88();
+    }
+}
+
+// 0x4606F0
+void sub_4606F0(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_8C != NULL) {
+        ui_callbacks.field_8C(a1, a2, a3, a4);
+    }
+}
+
+// 0x460720
+void sub_460720(int a1, int a2)
+{
+    if (ui_callbacks.field_90 != NULL) {
+        ui_callbacks.field_90(a1, a2);
+    }
+}
+
+// 0x460740
+void sub_460740(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_94 != NULL) {
+        ui_callbacks.field_94(a1, a2, a3, a4);
+    }
+}
+
+// 0x460770
+void sub_460770()
+{
+    if (ui_callbacks.field_98 != NULL) {
+        ui_callbacks.field_98();
+    }
+}
+
+// 0x460780
+void sub_460780()
+{
+    if (ui_callbacks.field_9C != NULL) {
+        ui_callbacks.field_9C();
+    }
+}
+
+// 0x460790
+void sub_460790(int a1, int a2)
+{
+    if (ui_callbacks.field_A0 != NULL) {
+        ui_callbacks.field_A0(a1, a2);
+    }
+}
+
+// 0x4607B0
+void sub_4607B0(int a1)
+{
+    if (ui_callbacks.field_A4 != NULL) {
+        ui_callbacks.field_A4(a1);
+    }
+}
+
+// 0x4607D0
+void sub_4607D0(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_A8 != NULL) {
+        ui_callbacks.field_A8(a1, a2, a3, a4);
+    }
+}
+
+// 0x460800
+void sub_460800(int a1, int a2)
+{
+    if (ui_callbacks.field_AC != NULL) {
+        ui_callbacks.field_AC(a1, a2);
+    }
+}
+
+// 0x460820
+void sub_460820(int a1, int a2)
+{
+    if (ui_callbacks.field_B0 != NULL) {
+        ui_callbacks.field_B0(a1, a2);
+    }
+}
+
+// 0x460840
+void sub_460840(int a1, int a2)
+{
+    if (ui_callbacks.field_B4 != NULL) {
+        ui_callbacks.field_B4(a1, a2);
+    }
+}
+
+// 0x460860
+void sub_460860()
+{
+    if (ui_callbacks.field_B8 != NULL) {
+        ui_callbacks.field_B8();
+    }
+}
+
+// 0x460870
+void sub_460870(int a1)
+{
+    if (ui_callbacks.field_BC != NULL) {
+        ui_callbacks.field_BC(a1);
+    }
+}
+
+// 0x460890
+void sub_460890(int a1)
+{
+    if (ui_callbacks.field_C0 != NULL) {
+        ui_callbacks.field_C0(a1);
+    }
+}
+
+// 0x4608B0
+void sub_4608B0(int a1, int a2, int a3)
+{
+    if (ui_callbacks.field_C4 != NULL) {
+        ui_callbacks.field_C4(a1, a2, a3);
+    }
+}
+
+// 0x4608D0
+void sub_4608D0(int a1, int a2, int a3, int a4, int a5)
+{
+    if (ui_callbacks.field_C8 != NULL) {
+        ui_callbacks.field_C8(a1, a2, a3, a4, a5);
+    }
+}
+
+// 0x460900
+void sub_460900(int a1, int a2, int a3, int a4, int a5)
+{
+    if (ui_callbacks.field_CC != NULL) {
+        ui_callbacks.field_CC(a1, a2, a3, a4, a5);
+    }
+}
+
+// 0x460930
+void sub_460930(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_D0 != NULL) {
+        ui_callbacks.field_D0(a1, a2, a3, a4);
+    }
+}
+
+// 0x460960
+void sub_460960(int a1)
+{
+    if (ui_callbacks.field_D4 != NULL) {
+        ui_callbacks.field_D4(a1);
+    }
+}
+
+// 0x460980
+void sub_460980(int a1, int a2)
+{
+    if (ui_callbacks.field_D8 != NULL) {
+        ui_callbacks.field_D8(a1, a2);
+    }
+}
+
+// 0x4609E0
+void sub_4609E0(int a1, int a2, int a3, int a4, int a5, int a6, int a7)
+{
+    if (ui_callbacks.field_E8 != NULL) {
+        ui_callbacks.field_E8(a1, a2, a3, a4, a5, a6, a7);
+    }
+}
+
+// 0x460A20
+void sub_460A20(int a1, int a2, int a3)
+{
+    if (ui_callbacks.field_EC != NULL) {
+        ui_callbacks.field_EC(a1, a2, a3);
+    }
+}
+
+// 0x460A40
+void sub_460A40(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8)
+{
+    if (ui_callbacks.field_F0 != NULL) {
+        ui_callbacks.field_F0(a1, a2, a3, a4, a5, a6, a7, a8);
+    }
+}
+
+// 0x460A80
+void sub_460A80(int a1, int a2, int a3, int a4, int a5, int a6)
+{
+    if (ui_callbacks.field_F4 != NULL) {
+        ui_callbacks.field_F4(a1, a2, a3, a4, a5, a6);
+    }
+}
+
+// 0x460AB0
+void sub_460AB0(int a1, int a2, int a3)
+{
+    if (ui_callbacks.field_F8 != NULL) {
+        ui_callbacks.field_F8(a1, a2, a3);
+    }
+}
+
+// 0x460AD0
+void sub_460AD0(int a1)
+{
+    if (ui_callbacks.field_FC != NULL) {
+        ui_callbacks.field_FC(a1);
+    }
+}
+
+// 0x460AF0
+void sub_460AF0()
+{
+    if (ui_callbacks.field_100 != NULL) {
+        ui_callbacks.field_100();
+    }
+}
+
+// 0x460B00
+int sub_460B00(int a1, int a2)
+{
+    if (ui_callbacks.field_104 != NULL) {
+        return ui_callbacks.field_104(a1, a2);
+    } else {
+        return 0;
+    }
+}
+
+// 0x460B20
+void sub_460B20()
+{
+    if (ui_callbacks.field_108 != NULL) {
+        ui_callbacks.field_108();
+    }
+}
+
+// 0x460B30
+int sub_460B30(int a1)
+{
+    if (ui_callbacks.field_10C != NULL) {
+        return ui_callbacks.field_10C(a1);
+    } else {
+        return 0;
+    }
+}
+
+// 0x460B50
+void sub_460B50(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_110 != NULL) {
+        ui_callbacks.field_110(a1, a2, a3, a4);
+    }
+}
+
+// 0x460B80
+void sub_460B80(int a1)
+{
+    if (ui_callbacks.field_114 != NULL) {
+        ui_callbacks.field_114(a1);
+    }
+}
+
+// 0x460BA0
+int sub_460BA0()
+{
+    if (ui_callbacks.field_118 != NULL) {
+        return ui_callbacks.field_118();
+    } else {
+        return 0;
+    }
+}
+
+// 0x460BB0
+int sub_460BB0()
+{
+    if (ui_callbacks.field_11C != NULL) {
+        return ui_callbacks.field_11C();
+    } else {
+        return 0;
+    }
+}
+
+// 0x460BC0
+int sub_460BC0()
+{
+    // FIXME: Checks the wrong callback for NULL.
+    if (ui_callbacks.field_11C != NULL) {
+        return ui_callbacks.field_120();
+    } else {
+        return 0;
+    }
+}
+
+// 0x460BE0
+int sub_460BE0(int a1, int a2)
+{
+    if (ui_callbacks.field_124 != NULL) {
+        return ui_callbacks.field_124(a1, a2);
+    } else {
+        return 0;
+    }
+}
+
+// 0x460C00
+void sub_460C00(int a1, int a2)
+{
+    if (ui_callbacks.field_128 != NULL) {
+        ui_callbacks.field_128(a1, a2);
+    }
+}
+
+// 0x460C20
+void sub_460C20()
+{
+    if (ui_callbacks.field_12C != NULL) {
+        ui_callbacks.field_12C();
+    }
+}
+
+// 0x460C30
+void sub_460C30(int a1, int a2)
+{
+    if (ui_callbacks.field_130 != NULL) {
+        ui_callbacks.field_130(a1, a2);
+    }
+}
+
+// 0x460C50
+int sub_460C50(int a1, int a2, int a3, int a4, int a5)
+{
+    if (ui_callbacks.field_134 != NULL) {
+        return ui_callbacks.field_134(a1, a2, a3, a4, a5);
+    } else {
+        return 0;
+    }
+}
+
+// 0x460C80
+int sub_460C80(int a1, int a2, int a3, int a4, int a5)
+{
+    if (ui_callbacks.field_138 != NULL) {
+        return ui_callbacks.field_138(a1, a2, a3, a4, a5);
+    } else {
+        return 0;
+    }
+}
+
+// 0x460CB0
+void sub_460CB0(int a1, int a2, int a3, int a4, int a5)
+{
+    if (ui_callbacks.field_13C != NULL) {
+        ui_callbacks.field_13C(a1, a2, a3, a4, a5);
+    }
+}
+
+// 0x460CE0
+int sub_460CE0(int a1, int a2, int a3, int a4, int a5)
+{
+    if (ui_callbacks.field_140 != NULL) {
+        return ui_callbacks.field_140(a1, a2, a3, a4, a5);
+    } else {
+        return 0;
+    }
+}
+
+// 0x460D10
+void sub_460D10(int a1)
+{
+    if (ui_callbacks.field_144 != NULL) {
+        ui_callbacks.field_144(a1);
+    }
+}
+
+// 0x460D30
+void sub_460D30(int a1)
+{
+    if (ui_callbacks.field_148 != NULL) {
+        ui_callbacks.field_148(a1);
+    }
+}
+
+// 0x460D50
+void sub_460D50(int a1, int a2)
+{
+    if (ui_callbacks.field_14C != NULL) {
+        ui_callbacks.field_14C(a1, a2);
+    }
+}
+
+// 0x460D70
+void sub_460D70(int a1, int a2)
+{
+    if (ui_callbacks.field_150 != NULL) {
+        ui_callbacks.field_150(a1, a2);
+    }
+}
+
+// 0x460D90
+void sub_460D90(int a1, int a2)
+{
+    if (ui_callbacks.field_154 != NULL) {
+        ui_callbacks.field_154(a1, a2);
+    }
+}
+
+// 0x460DB0
+void sub_460DB0(int a1, int a2)
+{
+    if (ui_callbacks.field_158 != NULL) {
+        ui_callbacks.field_158(a1, a2);
+    }
+}
+
+// 0x460DD0
+void sub_460DD0()
+{
+    if (ui_callbacks.field_15C != NULL) {
+        ui_callbacks.field_15C();
+    }
+}
+
+// 0x460DE0
+void sub_460DE0(int a1, int a2, int a3, int a4)
+{
+    if (ui_callbacks.field_160 != NULL) {
+        ui_callbacks.field_160(a1, a2, a3, a4);
+    }
+}
+
+// 0x460E10
+void sub_460E10(int a1)
+{
+    if (ui_callbacks.field_164 != NULL) {
+        ui_callbacks.field_164(a1);
+    }
+}
+
+// 0x460E30
+void sub_460E30(int a1, int a2, int a3)
+{
+    if (ui_callbacks.field_168 != NULL) {
+        ui_callbacks.field_168(a1, a2, a3);
+    }
+}
+
+// 0x460E50
+void sub_460E50(int a1, int a2, int a3)
+{
+    if (ui_callbacks.field_16C != NULL) {
+        ui_callbacks.field_16C(a1, a2, a3);
+    }
+}
