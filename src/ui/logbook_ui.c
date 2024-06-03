a607fb0d3f19e5e0ad79278b7598c7d04ebc4eaa
Alexander Batalov <alex.batalov@gmail.com>
2024-06-04 00:13:48 +0300

    Add logbook ui

---
+++ src/ui/logbook_ui.c
@@ -0,0 +1,670 @@
+#include "ui/logbook_ui.h"
+
+#include <tig/tig.h>
+
+#include "game/lib/curse.h"
+#include "game/lib/background.h"
+#include "game/lib/bless.h"
+#include "game/lib/description.h"
+#include "game/lib/timeevent.h"
+#include "game/lib/quest.h"
+
+// 0x5C33F0
+static tig_window_handle_t logbook_ui_window = TIG_WINDOW_HANDLE_INVALID;
+
+// 0x5C33F8
+static TigRect stru_5C33F8[2] = {
+    { 0, 41, 800, 400 },
+    { 150, 11, 501, 365 },
+};
+
+// 0x5C3418
+static TigRect stru_5C3418 = { 25, 24, 89, 89 };
+
+// 0x5C3428
+static UiButtonInfo stru_5C3428[2] = {
+    { 213, 77, 272, TIG_BUTTON_HANDLE_INVALID },
+    { 675, 77, 273, TIG_BUTTON_HANDLE_INVALID },
+};
+
+// 0x5C3448
+static UiButtonInfo stru_5C3448[7] = {
+    { 696, 83, 265, TIG_BUTTON_HANDLE_INVALID },
+    { 696, 129, 267, TIG_BUTTON_HANDLE_INVALID },
+    { 696, 175, 271, TIG_BUTTON_HANDLE_INVALID },
+    { 695, 221, 266, TIG_BUTTON_HANDLE_INVALID },
+    { 696, 267, 270, TIG_BUTTON_HANDLE_INVALID },
+    { 696, 313, 268, TIG_BUTTON_HANDLE_INVALID },
+    { 696, 359, 269, TIG_BUTTON_HANDLE_INVALID },
+};
+
+// 0x5C34B8
+static TigRect stru_5C34B8 = { 222, 72, 215, 270 };
+
+// 0x5C34C8
+static TigRect stru_5C34C8 = { 468, 72, 215, 270 };
+
+// 0x5C34D8
+static TigRect stru_5C34D8[2] = {
+    { 236, 43, 187, 19 },
+    { 482, 43, 187, 19 },
+};
+
+// 0x5C34F8
+static TigRect stru_5C34F8[2] = {
+    { 222, 346, 215, 15 },
+    { 468, 346, 215, 15 },
+};
+
+// 0x63FAE0
+static tig_font_handle_t dword_63FAE0;
+
+// 0x6429C4
+static tig_font_handle_t dword_6429C4;
+
+// 0x6429C8
+static tig_font_handle_t dword_6429C8;
+
+// 0x6429CC
+static int quotes_mes_file;
+
+// 0x6429D0
+static tig_font_handle_t dword_6429D0;
+
+// 0x64892C
+static tig_font_handle_t dword_64892C;
+
+// 0x648930
+static tig_font_handle_t dword_648930;
+
+// 0x648934
+static tig_font_handle_t dword_648934;
+
+// 0x64893C
+static tig_font_handle_t dword_64893C;
+
+// 0x648984
+static int logbook_ui_mes_file;
+
+// 0x64B868
+static bool logbook_ui_initialized;
+
+// 0x64B86C
+static bool logbook_ui_created;
+
+// 0x53EB10
+bool logbook_ui_init(GameInitInfo* init_info)
+{
+    TigFont font_info;
+    int index;
+
+    (void)init_info;
+
+    if (!message_load("mes\\logbk_ui.mes", &logbook_ui_mes_file)) {
+        return false;
+    }
+
+    if (!message_load("mes\\quotes.mes", &quotes_mes_file)) {
+        return false;
+    }
+
+    font_info.flags = 0;
+    tig_art_interface_id_create(229, 0, 0, 0, &(font_info.art_id));
+    font_info.str = NULL;
+    font_info.color = tig_color_make(0, 0, 0);
+    tig_font_create(&font_info, &dword_648934);
+
+    font_info.flags = TIG_FONT_CENTERED;
+    tig_art_interface_id_create(229, 0, 0, 0, &(font_info.art_id));
+    font_info.str = NULL;
+    font_info.color = tig_color_make(0, 0, 0);
+    tig_font_create(&font_info, &dword_6429C8);
+
+    font_info.flags = TIG_FONT_STRIKE_THROUGH;
+    tig_art_interface_id_create(229, 0, 0, 0, &(font_info.art_id));
+    font_info.str = NULL;
+    font_info.color = tig_color_make(0, 0, 0);
+    font_info.strike_through_color = font_info.color;
+    tig_font_create(&font_info, &dword_6429C4);
+
+    font_info.flags = TIG_FONT_STRIKE_THROUGH;
+    tig_art_interface_id_create(229, 0, 0, 0, &(font_info.art_id));
+    font_info.str = NULL;
+    font_info.color = tig_color_make(0, 120, 0);
+    font_info.strike_through_color = font_info.color;
+    tig_font_create(&font_info, &dword_63FAE0);
+
+    font_info.flags = TIG_FONT_STRIKE_THROUGH;
+    tig_art_interface_id_create(229, 0, 0, 0, &(font_info.art_id));
+    font_info.str = NULL;
+    font_info.color = tig_color_make(150, 0, 0);
+    font_info.strike_through_color = font_info.color;
+    tig_font_create(&font_info, &dword_64892C);
+
+    font_info.flags = TIG_FONT_CENTERED;
+    tig_art_interface_id_create(27, 0, 0, 0, &(font_info.art_id));
+    font_info.str = NULL;
+    font_info.color = tig_color_make(0, 0, 0);
+    tig_font_create(&font_info, &dword_648930);
+
+    font_info.flags = 0;
+    tig_art_interface_id_create(229, 0, 0, 0, &(font_info.art_id));
+    font_info.str = NULL;
+    font_info.color = tig_color_make(0, 0, 150);
+    tig_font_create(&font_info, &dword_64893C);
+
+    font_info.flags = 0;
+    tig_art_interface_id_create(229, 0, 0, 0, &(font_info.art_id));
+    font_info.str = NULL;
+    font_info.color = tig_color_make(150, 0, 0);
+    tig_font_create(&font_info, &dword_6429D0);
+
+    tig_font_push(dword_648934);
+    font_info.str = "test";
+    font_info.width = 0;
+    sub_535390(&font_info);
+    dword_63CBF0 = font_info.height;
+    tig_font_pop();
+
+    dword_64897C = -1;
+
+    for (index = 0; index < 100; index++) {
+        dword_64879C[index] = -1;
+    }
+    dword_64879C[0] = 0;
+
+    dword_648978 = 0;
+    qword_63FAD8 = 0;
+    dword_648798 = 1;
+    logbook_ui_initialized = 1;
+
+    return true;
+}
+
+// 0x53EF40
+void logbook_ui_exit()
+{
+    message_unload(logbook_ui_mes_file);
+    message_unload(quotes_mes_file);
+    tig_font_destroy(dword_648934);
+    tig_font_destroy(dword_6429C8);
+    tig_font_destroy(dword_6429C4);
+    tig_font_destroy(dword_63FAE0);
+    tig_font_destroy(dword_64892C);
+    tig_font_destroy(dword_648930);
+    tig_font_destroy(dword_64893C);
+    tig_font_destroy(dword_6429D0);
+    logbook_ui_initialized = 0;
+}
+
+// 0x53EFD0
+void logbook_ui_reset()
+{
+    int index;
+
+    if (logbook_ui_created) {
+        sub_53F090();
+    }
+
+    dword_648978 = 0;
+
+    for (index = 0; index < 100; index++) {
+        dword_64879C[index] = -1;
+    }
+    dword_64879C[0] = 0;
+
+    qword_63FAD8 = 0;
+    dword_64897C = -1;
+    dword_648798 = 1;
+}
+
+// 0x53F020
+void sub_53F020(long long obj)
+{
+    if (logbook_ui_created) {
+        sub_53F090();
+        return;
+    }
+
+    if (obj != OBJ_HANDLE_NULL
+        && !sub_45D8D0(player_get_pc_obj())
+        && sub_551A80(0)
+        && sub_551A80(7)) {
+        qword_63FAD8 = obj;
+        logbook_ui_create();
+    }
+
+
+}
+
+// 0x53F090
+void sub_53F090()
+{
+    if (logbook_ui_created
+        && sub_551A80(0)) {
+        logbook_ui_destroy();
+        qword_63FAD8 = OBJ_HANDLE_NULL;
+    }
+}
+
+// 0x53F0D0
+void sub_53F0D0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x53F0E0
+void logbook_ui_create()
+{
+    TigArtBlitSpec blit_info;
+    TigRect src_rect;
+    TigRect dst_rect;
+    int index;
+    tig_button_handle_t button_handles[7];
+    Jack v1;
+
+    if (logbook_ui_created) {
+        return;
+    }
+
+    if (!intgame_big_window_lock(logbook_ui_message_filter, &logbook_ui_window)) {
+        tig_debug_printf("logbk_ui_create: ERROR: intgame_big_window_lock failed!\n");
+        exit(EXIT_SUCCESS); // FIXME: Should be EXIT_FAILURE.
+    }
+
+    if (tig_kb_is_key_pressed(DIK_LCONTROL) && tig_kb_is_key_pressed(DIK_LMENU)) {
+        dword_648978 = 0;
+        dword_648798 = 1;
+        dword_648980 = 1;
+        gsound_play_sfx_id(3002, 1);
+    }
+
+    src_rect.x = 0;
+    src_rect.y = 0;
+    src_rect.width = stru_5C33F8.width;
+    src_rect.height = stru_5C33F8.height;
+
+    dst_rect.x = 0;
+    dst_rect.y = 0;
+    dst_rect.width = src_rect.width;
+    dst_rect.height = src_rect.height;
+
+    blit_info.flags = 0;
+    tig_art_interface_id_create(264, 0, 0, 0, &(blit_info.art_id));
+    blit_info.src_rect = &src_rect;
+    blit_info.dst_rect = &dst_rect;
+    tig_window_blit_art(logbook_ui_window, &blit_info);
+
+    for (index = 0; index < 2; index++) {
+        sub_54AA60(logbook_ui_window, &stru_5C33F8, &(stru_5C3428[index]), 9);
+    }
+
+    for (index = 0; index < 7; index++) {
+        sub_54AA60(logbook_ui_window, &stru_5C33F8, &(stru_5C3448[index]), 9);
+        button_handles[index] = stru_5C3448[index].button_handle;
+    }
+    tig_button_radio_group_create(7, button_handles, dword_648978);
+
+    sub_4B8CE0(obj_f_get_int64(qword_63FAD8, OBJ_F_LOCATION));
+
+    v1.window_handle = logbook_ui_window;
+    v1.rect = &stru_5C3418;
+    tig_art_interface_id_create(198, 0, 0, 0, &(v1.art_id));
+    sub_550DA0(1, &v1);
+    sub_53F490(261, 1);
+    sub_460790(1, 0);
+    gsound_play_sfx_id(3008, 1);
+
+    logbook_ui_created = true;
+}
+
+// 0x53F2F0
+void logbook_ui_destroy()
+{
+    if (!logbook_ui_created) {
+        return;
+    }
+
+    sub_550DA0(0, 0);
+    intgame_big_window_unlock();
+    logbook_ui_window = TIG_WINDOW_HANDLE_INVALID;
+    gsound_play_sfx_id(3009, 1);
+    logbook_ui_created = false;
+
+    if (dword_648980) {
+        dword_648798 = 1;
+    }
+    dword_648980 = false;
+}
+
+// 0x53F350
+bool logbook_ui_message_filter(TigMessage* msg)
+{
+    // TODO: Incomplete.
+}
+
+// 0x53F490
+void sub_53F490()
+{
+    // TODO: Incomplete.
+}
+
+// 0x53F5F0
+void sub_53F5F0(int a1, int a2)
+{
+    int index;
+
+    if (a2 == 0) {
+        for (index = 0; index < 100; index++) {
+            dword_64879C[index] = -1;
+        }
+        dword_64879C[0] = 0;
+        dword_648798 = 1;
+        dword_648978 = a1;
+    }
+
+    sub_53FBB0();
+    sub_53F6E0();
+    gsound_play_sfx_id(3010, 1);
+}
+
+// 0x53F640
+void sub_53F640()
+{
+    if (dword_648974 < dword_648938 - 1
+        && (dword_648798 - 1) / 2 < 100) {
+        dword_648798 += 2;
+        dword_64879C[(dword_648798 - 1) / 2] = dword_648974 + 1;
+        sub_53F6E0();
+        gsound_play_sfx_id(3010, 1);
+    }
+}
+
+// 0x53F6A0
+void sub_53F6A0()
+{
+    if (dword_64879C[(dword_648798 - 1) / 2] > 0) {
+        dword_648798 -= 2;
+        sub_53F6E0();
+        gsound_play_sfx_id(3010, 1);
+    }
+}
+
+// 0x53F6E0
+void sub_53F6E0()
+{
+    TigArtBlitSpec blit_info;
+    TigRect src_rect;
+    TigRect dst_rect;
+    MesFileEntry mes_file_entry;
+    int index;
+    char buffer[80];
+
+    tig_button_hide(stru_5C3428[0].button_handle);
+    tig_button_hide(stru_5C3428[1].button_handle);
+
+    src_rect.x = 0;
+    src_rect.y = 0;
+    src_rect.width = stru_5C33F8.width;
+    src_rect.height = stru_5C33F8.height;
+
+    dst_rect.x = 165;
+    dst_rect.y = 0;
+    dst_rect.width = src_rect.width;
+    dst_rect.height = src_rect.height;
+
+    blit_info.flags = 0;
+    tig_art_interface_id_create(260, 0, 0, 0, &(blit_info.art_id));
+    blit_info.src_rect = &src_rect;
+    blit_info.dst_rect = &dst_rect;
+
+    tig_window_blit_art(logbook_ui_window, &blit_info);
+
+    if (dword_648938 != 0) {
+        dword_648974 = sub_53F8F0(dword_64879C[(dword_648798 - 1) / 2], dword_648938 - 1);
+    } else {
+        dword_648974 = 0;
+    }
+
+    if (dword_64879C[(dword_648798 - 1) / 2] > 0) {
+        tig_button_show(stru_5C3428[0].button_handle);
+    }
+
+    if (dword_648974 < dword_648938 - 1) {
+        tig_button_show(stru_5C3428[1].button_handle);
+    }
+
+    mes_file_entry.num = dword_648978 || !dword_648980 ? dword_648978 : 100;
+    sub_4D43A0(logbook_ui_mes_file, &mes_file_entry);
+
+    tig_font_push(dword_648930);
+    for (index = 0; index < 2; index++) {
+        snprintf(buffer, sizeof(buffer), "%c%s%c", '-', mes_file_entry.text, '-');
+        tig_window_text_write(logbook_ui_window, buffer, &(stru_5C34D8[index]);
+    }
+    tig_font_pop();
+
+    tig_font_push(dword_6429C8);
+    for (index = 0; index < 2; index++) {
+        snprintf(buffer, sizeof(buffer), "%c%d%c", '-', dword_648798 + index, '-');
+        tig_window_text_write(logbook_ui_window, buffer, &(stru_5C34F8[index]);
+    }
+    tig_font_pop();
+}
+
+// 0x53F8F0
+void sub_53F8F0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x53F9E0
+void sub_53F9E0(int index, int a2, int a3, int a4)
+{
+    char buffer[2000];
+    bool v1;
+
+    v1 = a1 < dword_648938 - 1;
+    buffer[0] = '\0';
+
+    switch (dword_648978) {
+    case 0:
+        sub_540310(buffer, index);
+        break;
+    case 1:
+        sub_540470(buffer, index);
+        break;
+    case 2:
+        sub_540510(buffer, index);
+        break;
+    case 3:
+        sub_540550(buffer, index);
+        break;
+    case 4:
+        sub_5405C0(buffer, index);
+        v1 = false;
+        break;
+    case 5:
+        sub_540760(buffer, index);
+        break;
+    case 6:
+        sub_5407B0(buffer, index);
+        break;
+    default:
+        sub_53FAD0(buffer, dword_648988[index], a2, a3, a4, v1);
+        break;
+    }
+}
+
+// 0x53FAD0
+void sub_53FAD0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x53FBB0
+void sub_53FBB0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x540310
+void sub_540310()
+{
+    // TODO: Incomplete.
+}
+
+// 0x5403C0
+void sub_5403C0(char* buffer, int index)
+{
+    DateTime* datetime;
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+    char am_pm;
+    MesFileEntry mes_file_entry;
+
+    datetime = &(stru_6429D8[index]);
+    month = datetime_get_month(&datetime);
+    day = datetime_get_day(&datetime);
+    year = datetime_get_year(&datetime);
+    hour = datetime_get_hour(&datetime);
+    minute = datetime_get_minute(&datetime);
+
+    if (hour < 12) {
+        am_pm = 'a';
+        if (hour == 0) {
+            hour = 12;
+        }
+    } else {
+        am_pm = 'p';
+        if (hour > 12) {
+            hour -= 12;
+        }
+    }
+
+    mes_file_entry.num = month + 16;
+    sub_4D43A0(logbook_ui_mes_file, &mes_file_entry);
+
+    sprintf(buffer, "%s %d, %d  %d:%.2d%cm    ",
+        mes_file_entry.text,
+        day,
+        year,
+        hour,
+        minute,
+        am_pm);
+}
+
+// 0x540470
+void sub_540470(char* buffer, int index)
+{
+    MesFileEntry mes_file_entry;
+    size_t pos;
+
+    sub_5403C0(buffer, index);
+    mes_file_entry.num = dword_63CBF4[index] + 19;
+    sub_4D43A0(logbook_ui_mes_file, &mes_file_entry);
+    strcat(buffer, mes_file_entry.text);
+
+    pos = strlen(buffer);
+    buffer[pos] = '\n';
+
+    quest_copy_description(qword_63FAD8, dword_63FAE4[index], &(buffer[pos + 1]));
+}
+
+// 0x540510
+void sub_540510(char* buffer, int index)
+{
+    size_t pos;
+
+    sub_5403C0(buffer, index);
+    pos = strlen(buffer);
+    buffer[pos] = '\n';
+
+    sub_4C1BD0(dword_63FAE4[index], &(buffer[pos + 1]));
+}
+
+// 0x540550
+void sub_540550(char* buffer, int index)
+{
+    size_t pos;
+
+    sub_5403C0(buffer, index);
+    pos = strlen(buffer);
+    buffer[pos] = '\n';
+
+    if (dword_648988[index] == dword_64893C) {
+        bless_copy_name(dword_63FAE4[index], &(buffer[pos + 1]));
+    } else {
+        curse_copy_name(dword_63FAE4[index], &(buffer[pos + 1]));
+    }
+}
+
+// 0x5405C0
+void sub_5405C0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x540760
+void sub_540760(char* buffer, int index)
+{
+    const char* body;
+    int start;
+    int end;
+
+    body = background_description_get_body(dword_63FAE4[0]);
+    if (index != 0) {
+        start = dword_63FAE4[index];
+    } else {
+        start = 0;
+    }
+
+    end = dword_63FAE4[index + 1] - start;
+    strncpy(buffer, &(body[start]), end);
+    buffer[end] = '\0';
+}
+
+// 0x5407B0
+void sub_5407B0(char* buffer, int index);
+{
+    strcpy(buffer, description_get_key_name(dword_63FAE4[index]));
+}
+
+// 0x5407F0
+void sub_5407F0()
+{
+    int index;
+    char buffer[2000];
+    size_t pos;
+    TigRect rect;
+
+    stru_6429D8 = sub_45A7C0();
+
+    for (index = 0; index < 3000; index++) {
+        rect = stru_5C34B8;
+
+        sub_5403C0(buffer, 0);
+        pos = strlen(buffer);
+        buffer[pos] = '\n';
+
+        sub_4C5960(index, &(buffer[pos + 1]));
+        if (buffer[pos + 1] != '\0') {
+            tig_debug_printf("Checking rumor %d\n", index);
+            sub_53FAD0(buffer, dword_648934, &rect, 1, 1, 1);
+        }
+
+        rect = stru_5C34B8;
+
+        sub_5403C0(buffer, 0);
+        pos = strlen(buffer);
+        buffer[pos] = '\n';
+        sub_4C59D0(index, &(buffer[pos + 1]));
+        if (buffer[pos + 1] != '\0') {
+            tig_debug_printf("Checking dumb rumor %d\n", index);
+            sub_53FAD0(buffer, dword_648934, &rect, 1, 1, 1);
+        }
+    }
+}
