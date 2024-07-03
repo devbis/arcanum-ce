a6733a7a123307cff831c07be34cc92c1a3d7457
Alexander Batalov <alex.batalov@gmail.com>
2024-07-03 21:20:23 +0300

    Add object_set_hp_adj

--- src/game/object.c
+++ src/game/object.c
@@ -13,3 +13,11 @@ int object_get_hp_adj(object_id_t obj)
 {
     return obj_f_get_int32(obj, OBJ_F_HP_ADJ);
 }
+
+// 0x43D4E0
+int object_set_hp_adj(object_id_t obj, int value)
+{
+    obj_f_set_int32(obj, OBJ_F_HP_ADJ, value);
+    sub_460240(obj);
+    return value;
+}
