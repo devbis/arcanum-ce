e211334bea6375accfa334574df532cd4469b4ee
Alexander Batalov <alex.batalov@gmail.com>
2024-07-03 20:32:01 +0300

    Add sub_4395C0

--- src/game/roof.c
+++ src/game/roof.c
@@ -14,3 +14,9 @@ void roof_exit()
     dword_5E2E44 = TIG_WINDOW_HANDLE_INVALID;
     dword_5E2E34 = NULL;
 }
+
+// 0x4395C0
+int sub_4395C0(int a1)
+{
+    return ((a1 >> 2) & 0xF) + ((a1 >> 8) << 4);
+}
