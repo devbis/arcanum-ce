1006b32a8b248201bf78b07d1925906bd8e24e70
Alexander Batalov <alex.batalov@gmail.com>
2024-07-03 21:23:49 +0300

    Add object_set_hp_damage

--- src/game/object.c
+++ src/game/object.c
@@ -1,5 +1,7 @@
 #include "game/object.h"
 
+#include <tig/tig.h>
+
 #include "game/lib/object.h"
 
 // 0x43D410
@@ -26,4 +28,31 @@ int object_set_hp_adj(object_id_t obj, int value)
 int object_get_hp_damage(object_id_t obj)
 {
     return obj_f_get_int32(obj, OBJ_F_HP_DAMAGE);
+}
+
+// 0x43D530
+int object_set_hp_damage(object_id_t obj, int value)
+{
+    int type;
+
+    if ((tig_net_flags & 0x1) != 0 && (tig_net_flags & 0x2) == 0) {
+        return 0;
+    }
+
+    if (value < 0) {
+        value = 0;
+    }
+
+    sub_4EFDD0(obj, OBJ_F_HP_DAMAGE, value);
+
+    if (value > 0) {
+        type = obj_f_get_int32(obj, OBJ_F_TYPE);
+        if (type == OBJ_TYPE_PC || type == OBJ_TYPE_NPC) {
+            sub_45EAB0(obj);
+        }
+    }
+
+    sub_4EE1D0(obj);
+
+    return value;
 }
