cef704746cd49943e0a8fcfe4ef45bd5c28162cd
Alexander Batalov <alex.batalov@gmail.com>
2024-07-03 21:19:21 +0300

    Add object_get_hp_adj

--- src/game/object.c
+++ src/game/object.c
@@ -7,3 +7,9 @@ int object_get_hp_pts(object_id_t obj)
 {
     return obj_f_get_int32(obj, OBJ_F_HP_PTS);
 }
+
+// 0x43D4C0
+int object_get_hp_adj(object_id_t obj)
+{
+    return obj_f_get_int32(obj, OBJ_F_HP_ADJ);
+}
