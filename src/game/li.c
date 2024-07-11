04551627a5075a4de461e80b94f60d8ecaf16991
Alexander Batalov <alex.batalov@gmail.com>
2024-07-11 18:51:13 +0300

    Review li

--- src/game/li.c
+++ src/game/li.c
@@ -1,4 +1,4 @@
-#include "game/lib/li.h"
+#include "game/li.h"
 
 #include "tig/art.h"
 #include "tig/color.h"
@@ -6,67 +6,68 @@
 #include "tig/video.h"
 #include "tig/window.h"
 
-static void sub_4BBCE0();
+static void li_blit();
 
 // 0x5FC3F8
-static unsigned int dword_5FC3F8;
+static tig_art_id_t li_indicator_art_id;
 
 // 0x5FC400
-static TigRect stru_5FC400;
+static TigRect li_indicator_frame;
 
 // 0x5FC410
-static TigArtBlitSpec stru_5FC410;
+static TigArtBlitSpec li_indicator_blit_info;
 
 // 0x5FC43C
 static int dword_5FC43C;
 
 // 0x5FC440
-static TigVideoBuffer* off_5FC440;
+static TigVideoBuffer* li_indicator_vb;
 
 // 0x5FC448
-static TigRect stru_5FC448;
+static TigRect li_indicator_bounds;
 
 // 0x4BB9A0
-bool li_init(GameContext* ctx)
+bool li_init(GameContext* init_info)
 {
-    tig_art_interface_id_create(601, 0, 0, 0, &dword_5FC3F8);
-
     TigArtFrameData art_frame_data;
-    if (tig_art_frame_data(dword_5FC3F8, &art_frame_data) != TIG_OK) {
-        return false;
-    }
-
     TigWindowData window_data;
-    if (tig_window_data(ctx->iso_window_handle, &window_data) != TIG_OK) {
+    TigVideoBufferCreateInfo vb_create_info;
+
+    tig_art_interface_id_create(601, 0, 0, 0, &li_indicator_art_id);
+
+    if (tig_art_frame_data(li_indicator_art_id, &art_frame_data) != TIG_OK) {
         return false;
     }
 
-    TigVideoBufferCreateInfo vb_create_info;
+    if (tig_window_data(init_info->iso_window_handle, &window_data) != TIG_OK) {
+        return false;
+    }
+
     vb_create_info.width = art_frame_data.width;
     vb_create_info.height = art_frame_data.height;
     vb_create_info.flags = TIG_VIDEO_BUFFER_CREATE_COLOR_KEY | TIG_VIDEO_BUFFER_CREATE_VIDEO_MEMORY;
     vb_create_info.color_key = tig_color_make(0, 0, 255);
     vb_create_info.background_color = vb_create_info.color_key;
-    if (tig_video_buffer_create(&vb_create_info, &off_5FC440) != TIG_OK) {
+    if (tig_video_buffer_create(&vb_create_info, &li_indicator_vb) != TIG_OK) {
         return false;
     }
 
-    stru_5FC448.x = 0;
-    stru_5FC448.y = 0;
-    stru_5FC448.width = art_frame_data.width;
-    stru_5FC448.height = art_frame_data.height;
+    li_indicator_bounds.x = 0;
+    li_indicator_bounds.y = 0;
+    li_indicator_bounds.width = art_frame_data.width;
+    li_indicator_bounds.height = art_frame_data.height;
 
-    stru_5FC400.x = window_data.rect.width - art_frame_data.width + window_data.rect.x - 2;
-    stru_5FC400.y = window_data.rect.y + 2;
-    stru_5FC400.width = art_frame_data.width;
-    stru_5FC400.height = art_frame_data.height;
+    li_indicator_frame.x = window_data.rect.width - art_frame_data.width + window_data.rect.x - 2;
+    li_indicator_frame.y = window_data.rect.y + 2;
+    li_indicator_frame.width = art_frame_data.width;
+    li_indicator_frame.height = art_frame_data.height;
 
-    stru_5FC410.field_0 = 0;
-    stru_5FC410.src_rect = &stru_5FC448;
-    stru_5FC410.dst_video_buffer = off_5FC440;
-    stru_5FC410.dst_rect = &stru_5FC448;
+    li_indicator_blit_info.flags = 0;
+    li_indicator_blit_info.src_rect = &li_indicator_bounds;
+    li_indicator_blit_info.dst_video_buffer = li_indicator_vb;
+    li_indicator_blit_info.dst_rect = &li_indicator_bounds;
 
-    sub_4BBCE0();
+    li_blit();
 
     return true;
 }
@@ -74,40 +75,41 @@ bool li_init(GameContext* ctx)
 // 0x4BBB00
 void li_exit()
 {
-    if (off_5FC440 != NULL) {
-        tig_video_buffer_destroy(off_5FC440);
-        off_5FC440 = NULL;
+    if (li_indicator_vb != NULL) {
+        tig_video_buffer_destroy(li_indicator_vb);
+        li_indicator_vb = NULL;
     }
 }
 
 // 0x4BBB20
-void li_resize(ResizeContext* ctx)
+void li_resize(ResizeInfo* resize_info)
 {
-    tig_art_interface_id_create(601, 0, 0, 0, &dword_5FC3F8);
-
     TigArtFrameData art_frame_data;
-    if (tig_art_frame_data(dword_5FC3F8, &art_frame_data) != TIG_OK) {
+    TigWindowData window_data;
+
+    tig_art_interface_id_create(601, 0, 0, 0, &li_indicator_art_id);
+
+    if (tig_art_frame_data(li_indicator_art_id, &art_frame_data) != TIG_OK) {
         tig_debug_printf("li_resize: ERROR: couldn't grab frame data!\n");
         exit(EXIT_FAILURE);
     }
 
-    TigWindowData window_data;
-    if (tig_window_data(ctx->iso_window_handle, &window_data) != TIG_OK) {
+    if (tig_window_data(resize_info->iso_window_handle, &window_data) != TIG_OK) {
         tig_debug_printf("li_resize: ERROR: couldn't grab window data!");
         exit(EXIT_FAILURE);
     }
 
-    stru_5FC448.x = 0;
-    stru_5FC448.y = 0;
-    stru_5FC448.width = art_frame_data.width;
-    stru_5FC448.height = art_frame_data.height;
+    li_indicator_bounds.x = 0;
+    li_indicator_bounds.y = 0;
+    li_indicator_bounds.width = art_frame_data.width;
+    li_indicator_bounds.height = art_frame_data.height;
 
-    stru_5FC400.x = window_data.rect.width - art_frame_data.width + window_data.rect.x - 2;
-    stru_5FC400.y = window_data.rect.y + 2;
-    stru_5FC400.width = art_frame_data.width;
-    stru_5FC400.height = art_frame_data.height;
+    li_indicator_frame.x = window_data.rect.width - art_frame_data.width + window_data.rect.x - 2;
+    li_indicator_frame.y = window_data.rect.y + 2;
+    li_indicator_frame.width = art_frame_data.width;
+    li_indicator_frame.height = art_frame_data.height;
 
-    sub_4BBCE0();
+    li_blit();
 }
 
 // 0x4BBC00
@@ -129,26 +131,26 @@ int sub_4BBC20()
 }
 
 // 0x4BBC30
-void sub_4BBC30()
+void li_update()
 {
     if (dword_5FC43C <= 0) {
-        tig_video_blit(off_5FC440, &stru_5FC448, &stru_5FC400, 1);
-        dword_5FC3F8 = tig_art_id_frame_inc(dword_5FC3F8);
-        sub_4BBCE0();
-        tig_window_set_needs_display_in_rect(&stru_5FC400);
+        tig_video_blit(li_indicator_vb, &li_indicator_bounds, &li_indicator_frame, 1);
+        li_indicator_art_id = tig_art_id_frame_inc(li_indicator_art_id);
+        li_blit();
+        tig_window_set_needs_display_in_rect(&li_indicator_frame);
     }
 }
 
 // 0x4BBC80
-void sub_4BBC80()
+void li_redraw()
 {
-    tig_video_buffer_fill(off_5FC440, &stru_5FC448, tig_color_make(0, 0, 255));
-    sub_4BBCE0();
+    tig_video_buffer_fill(li_indicator_vb, &li_indicator_bounds, tig_color_make(0, 0, 255));
+    li_blit();
 }
 
 // 0x4BBCE0
-static void sub_4BBCE0()
+void li_blit()
 {
-    stru_5FC410.src_art_id = dword_5FC3F8;
-    tig_art_blit(&stru_5FC410);
+    li_indicator_blit_info.art_id = li_indicator_art_id;
+    tig_art_blit(&li_indicator_blit_info);
 }
--- src/game/li.h
+++ src/game/li.h
@@ -1,15 +1,15 @@
-#ifndef ARCANUM_GAMELIB_LI_H_
-#define ARCANUM_GAMELIB_LI_H_
+#ifndef ARCANUM_GAME_LI_H_
+#define ARCANUM_GAME_LI_H_
 
 #include "game/context.h"
 
-bool li_init(GameContext* ctx);
+bool li_init(GameInitInfo* init_info);
 void li_exit();
-void li_resize(ResizeContext* ctx);
+void li_resize(ResizeInfo* resize_info);
 void sub_4BBC00();
 void sub_4BBC10();
 int sub_4BBC20();
-void sub_4BBC30();
-void sub_4BBC80();
+void li_update();
+void li_redraw();
 
-#endif /* ARCANUM_GAMELIB_LI_H_ */
+#endif /* ARCANUM_GAME_LI_H_ */
