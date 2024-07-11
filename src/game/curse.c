79ba32a141f71c9d20606845d42d4a924b11970d
Alexander Batalov <alex.batalov@gmail.com>
2024-07-11 19:29:16 +0300

    Review curse

--- src/game/curse.c
+++ src/game/curse.c
@@ -1,60 +1,61 @@
-#include "game/lib/curse.h"
+#include "game/curse.h"
 
-#include "game/lib/message.h"
+#include "game/mes.h"
 
-#define CURSE_FIELD_NAME 0
-#define CURSE_FIELD_EFFECT 1
-#define CURSE_FIELD_DESCRIPTION 2
+#define CURSE_F_NAME 0
+#define CURSE_F_EFFECT 1
+#define CURSE_F_DESCRIPTION 2
 
-static void curse_copy_field(int curse, int field, char* dest);
+static void curse_copy_field(int curse, int field, char* buffer);
 
 // 0x5FF40C
-static int curse_msg_file;
+static int curse_mes_file;
 
 // 0x4C3C60
 bool curse_mod_load()
 {
-    message_load("mes\\gamecurse.mes", &curse_msg_file);
+    mes_load("mes\\gamecurse.mes", &curse_mes_file);
     return true;
 }
 
 // 0x4C3C80
 void curse_mod_unload()
 {
-    message_unload(curse_msg_file);
-    curse_msg_file = -1;
+    mes_unload(curse_mes_file);
+    curse_mes_file = MES_FILE_HANDLE_INVALID;
 }
 
 // 0x4C3CA0
-void curse_copy_name(int curse, char* dest)
+void curse_copy_name(int curse, char* buffer)
 {
-    curse_copy_field(curse, CURSE_FIELD_NAME, dest);
+    curse_copy_field(curse, CURSE_F_NAME, buffer);
 }
 
 // 0x4C3CC0
-void curse_copy_field(int curse, int field, char* dest)
+void curse_copy_field(int curse, int field, char* buffer)
 {
-    dest[0] = '\0';
+    MesFileEntry mes_file_entry;
+
+    buffer[0] = '\0';
 
     if (curse >= 50) {
-        MessageListItem message_list_item;
-        message_list_item.num = curse * 10 + field;
-        if (message_find(curse_msg_file, &message_list_item)) {
-            strcpy(dest, message_list_item.text);
+        mes_file_entry.num = curse * 10 + field;
+        if (mes_search(curse_mes_file, &mes_file_entry)) {
+            strcpy(buffer, mes_file_entry.str);
         }
     }
 }
 
 // 0x4C3D30
-void curse_copy_description(int curse, char* dest)
+void curse_copy_description(int curse, char* buffer)
 {
-    curse_copy_field(curse, CURSE_FIELD_DESCRIPTION, dest);
+    curse_copy_field(curse, CURSE_F_DESCRIPTION, buffer);
 }
 
 // 0x4C3F70
 int curse_get_effect(int curse)
 {
     char buffer[80];
-    curse_copy_field(curse, CURSE_FIELD_EFFECT, buffer);
+    curse_copy_field(curse, CURSE_F_EFFECT, buffer);
     return atoi(buffer);
 }
--- src/game/curse.h
+++ src/game/curse.h
@@ -1,12 +1,12 @@
-#ifndef ARCANUM_GAME_LIB_CURSE_H_
-#define ARCANUM_GAME_LIB_CURSE_H_
+#ifndef ARCANUM_GAME_CURSE_H_
+#define ARCANUM_GAME_CURSE_H_
 
 #include "game/context.h"
 
 bool curse_mod_load();
 void curse_mod_unload();
-void curse_copy_name(int curse, char* dest);
-void curse_copy_description(int curse, char* dest);
+void curse_copy_name(int curse, char* buffer);
+void curse_copy_description(int curse, char* buffer);
 int curse_get_effect(int curse);
 
-#endif /* ARCANUM_GAME_LIB_CURSE_H_ */
+#endif /* ARCANUM_GAME_CURSE_H_ */
