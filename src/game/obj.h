d24279101dccb45fb24ce1ca4621a44bfdd75255
Alexander Batalov <alex.batalov@gmail.com>
2024-07-13 07:16:17 +0300

    Clarify sub_40C260 signature

--- src/game/obj.h
+++ src/game/obj.h
@@ -789,7 +789,7 @@ int object_field_get(object_id_t object_id, int field);
 void object_field_set(object_id_t object_id, int field, int value);
 long long object_field_get_64(object_id_t object_id, int field);
 int sub_40C030(ObjectType object_type);
-bool sub_40C260(ObjectType object_type, ObjectField field);
+bool sub_40C260(int type, int fld);
 bool obj_enumerate_fields(Object* object, ObjEnumerateCallback* callback);
 int64_t obj_get_prototype_handle(Object* object);
 void object_field_set_with_network(object_id_t object_id, int field, int a3, int a4);
