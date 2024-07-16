811d3c4f6629c812a5423887129ece4b665ebe7a
Alexander Batalov <alex.batalov@gmail.com>
2024-07-16 19:22:41 +0300

    Add sub_589540

--- src/ui/options_ui.c
+++ src/ui/options_ui.c
@@ -1,7 +1,16 @@
 #include "ui/options_ui.h"
 
+static void sub_589540(int* a1, bool* a2);
+
 // 0x589530
 int sub_589530(int a1)
 {
     return sub_57F6D0(a1);
+}
+
+// 0x589540
+void sub_589540(int* a1, bool* a2)
+{
+    *a1 = dword_68726C;
+    *a2 = dword_687260 == 0;
 }
