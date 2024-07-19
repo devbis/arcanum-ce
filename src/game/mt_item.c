86667bbcb07e06df23f3901d99b113b94f699757
Alexander Batalov <alex.batalov@gmail.com>
2024-07-19 19:04:29 +0300

    Add mt_item_spell

--- src/game/mt_item.c
+++ src/game/mt_item.c
@@ -1,6 +1,7 @@
 #include "game/mt_item.h"
 
 #include "game/magictech.h"
+#include "game/obj.h"
 
 // 0x5FF610
 static bool mt_item_initialized;
@@ -33,4 +34,21 @@ int mt_item_triggers(int index)
 int sub_4CB790(int a1)
 {
     return a1 & 0xFF;
+}
+
+// 0x4CB7A0
+int mt_item_spell(int64_t obj_handle, int index)
+{
+    int spl;
+
+    if (obj_handle == OBJ_HANDLE_NULL) {
+        return -1;
+    }
+
+    spl = obj_f_get_int32(obj_handle, OBJ_F_ITEM_SPELL_1 + index);
+    if (spl == 10000) {
+        return -1;
+    }
+
+    return spl;
 }
--- src/game/mt_item.h
+++ src/game/mt_item.h
@@ -7,5 +7,6 @@ bool mt_item_init(GameInitInfo* init_info);
 void mt_item_exit();
 int mt_item_triggers(int index);
 int sub_4CB790(int a1);
+int mt_item_spell(int64_t obj_handle, int index);
 
 #endif /* ARCANUM_GAME_MT_ITEM_H_ */
