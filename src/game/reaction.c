d73a33d4a0e6b681e61ff27084addb67ba45147c
Alexander Batalov <alex.batalov@gmail.com>
2024-07-20 16:17:50 +0300

    Clarify reaction_mes_file

--- src/game/reaction.c
+++ src/game/reaction.c
@@ -4,7 +4,7 @@
 #include "game/obj.h"
 
 // 0x5FC88C
-static int reaction_msg_file;
+static mes_file_handle_t reaction_mes_file;
 
 // 0x5FC890
 static char* reaction_names[REACTION_COUNT];
@@ -17,13 +17,13 @@ bool reaction_init(GameInitInfo* init_info)
 
     (void)init_info;
 
-    if (!message_load("mes\\reaction.mes", &reaction_msg_file)) {
+    if (!mes_load("mes\\reaction.mes", &reaction_mes_file)) {
         return false;
     }
 
     for (index = 0; index < REACTION_COUNT; index++) {
         mes_file_entry.num = index;
-        mes_get_msg(reaction_msg_file, &mes_file_entry);
+        mes_get_msg(reaction_mes_file, &mes_file_entry);
         reaction_names[index] = mes_file_entry.str;
     }
 
@@ -33,7 +33,7 @@ bool reaction_init(GameInitInfo* init_info)
 // 0x4C0C30
 void reaction_exit()
 {
-    message_unload(reaction_msg_file);
+    mes_unload(reaction_mes_file);
 }
 
 // 0x4C0FC0
