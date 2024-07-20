73a3a847f61838fc7434cf99a053dadd3ff3d15a
Alexander Batalov <alex.batalov@gmail.com>
2024-07-20 11:56:51 +0300

    Add object_field_indexed_int64_get

--- src/game/obj.c
+++ src/game/obj.c
@@ -1577,6 +1577,25 @@ void sub_4074E0(int64_t obj_handle, int fld, int index, int value)
     obj_unlock(obj_handle);
 }
 
+// 0x407540
+int64_t object_field_indexed_int64_get(int64_t obj_handle, int fld, int index)
+{
+    Object* object;
+    int64_t value;
+
+    object = obj_lock(obj_handle);
+    if (!sub_40C260(object->type, fld)) {
+        object_field_not_exists(object, fld);
+        obj_unlock(obj_handle);
+        return 0;
+    }
+
+    sub_408BB0(object, fld, index, &value);
+    obj_unlock(obj_handle);
+
+    return value;
+}
+
 // 0x408430
 void sub_408430(tig_art_id_t aid)
 {
