0f69e9a3597dc9c8936e80a981eda2967a221cea
Alexander Batalov <alex.batalov@gmail.com>
2024-07-26 00:08:57 +0300

    Review gmovie

--- src/game/gmovie.c
+++ src/game/gmovie.c
@@ -1,18 +1,18 @@
-#include "game/lib/gmovie.h"
+#include "game/gmovie.h"
 
 #include <stdio.h>
 
-#include "game/lib/message.h"
-#include "tig/movie.h"
+#include "game/gsound.h"
+#include "game/mes.h"
 
 // 0x59F02C
-static int movies_msg_file = -1;
+static mes_file_handle_t movies_mes_file = MES_FILE_HANDLE_INVALID;
 
 // 0x40DE20
 bool gmovie_mod_load()
 {
-    if (!message_load("movies\\movies.mes", &movies_msg_file)) {
-        movies_msg_file = -1;
+    if (!mes_load("movies\\movies.mes", &movies_mes_file)) {
+        movies_mes_file = MES_FILE_HANDLE_INVALID;
     }
 
     return true;
@@ -21,21 +21,22 @@ bool gmovie_mod_load()
 // 0x40DE50
 void gmovie_mod_unload()
 {
-    if (movies_msg_file != -1) {
-        message_unload(movies_msg_file);
-        movies_msg_file = -1;
+    if (movies_mes_file != MES_FILE_HANDLE_INVALID) {
+        mes_unload(movies_mes_file);
+        movies_mes_file = MES_FILE_HANDLE_INVALID;
     }
 }
 
 // 0x40DE70
 void gmovie_play(int movie, unsigned int flags, int sound_track)
 {
-    if (movies_msg_file != -1) {
-        MessageListItem msg;
-        msg.num = movie;
-        if (message_find(movies_msg_file, &msg)) {
-            char path[MAX_PATH];
-            sprintf(path, "movies\\%s", msg.text);
+    MesFileEntry mes_file_entry;
+    char path[MAX_PATH];
+
+    if (movies_mes_file != MES_FILE_HANDLE_INVALID) {
+        mes_file_entry.num = movie;
+        if (mes_search(movies_mes_file, &mes_file_entry)) {
+            sprintf(path, "movies\\%s", mes_file_entry.str);
             gmovie_play_path(path, flags, sound_track);
         }
     }
@@ -45,25 +46,27 @@ void gmovie_play(int movie, unsigned int flags, int sound_track)
 void gmovie_play_path(const char* path, unsigned int flags, int sound_track)
 {
     char temp_path[MAX_PATH];
+    unsigned int movie_flags;
+
     if (tig_file_extract(path, temp_path)) {
         sub_41C610();
 
-        unsigned int movie_flags = 0;
+        movie_flags = 0;
 
         if ((flags & GAME_MOVIE_FADE_IN) != 0) {
-            movie_flags |= MOVIE_FADE_IN;
+            movie_flags |= TIG_MOVIE_FADE_IN;
         }
 
         if ((flags & GAME_MOVIE_FADE_OUT) != 0) {
-            movie_flags |= MOVIE_FADE_OUT;
+            movie_flags |= TIG_MOVIE_FADE_OUT;
         }
 
-        if ((flags & GAME_MOVIE_FLAG_0x04) != 0) {
-            movie_flags |= MOVIE_FLAG_0x01;
+        if ((flags & GAME_MOVIE_IGNORE_KEYBOARD) != 0) {
+            movie_flags |= TIG_MOVIE_IGNORE_KEYBOARD;
         }
 
-        if ((flags & GAME_MOVIE_FLAG_0x08) != 0) {
-            movie_flags |= MOVIE_FLAG_0x08;
+        if ((flags & GAME_MOVIE_BLACK_OUT) != 0) {
+            movie_flags |= TIG_MOVIE_BLACK_OUT;
         }
 
         tig_movie_play(temp_path, movie_flags, sound_track);
--- src/game/gmovie.h
+++ src/game/gmovie.h
@@ -1,18 +1,19 @@
-#ifndef ARCANUM_GAMELIB_GMOVIE_H_
-#define ARCANUM_GAMELIB_GMOVIE_H_
+#ifndef ARCANUM_GAME_GMOVIE_H_
+#define ARCANUM_GAME_GMOVIE_H_
 
 #include "game/context.h"
 
+// NOTE: Gmovie flags are the same as TIG but have slightly different values.
 typedef enum GameMovieFlags {
     GAME_MOVIE_FADE_IN = 0x1,
     GAME_MOVIE_FADE_OUT = 0x2,
-    GAME_MOVIE_FLAG_0x04 = 0x4,
-    GAME_MOVIE_FLAG_0x08 = 0x8,
-};
+    GAME_MOVIE_IGNORE_KEYBOARD = 0x4,
+    GAME_MOVIE_BLACK_OUT = 0x8,
+} GameMovieFlags;
 
 bool gmovie_mod_load();
 void gmovie_mod_unload();
 void gmovie_play(int movie, unsigned int flags, int sound_track);
 void gmovie_play_path(const char* path, unsigned int flags, int sound_track);
 
-#endif /* ARCANUM_GAMELIB_GMOVIE_H_ */
+#endif /* ARCANUM_GAME_GMOVIE_H_ */
