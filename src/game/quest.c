e9f53035163a0234c375ca71892a698bbd3cdd09
Alexander Batalov <alex.batalov@gmail.com>
2024-07-25 22:30:56 +0300

    Review quest

--- src/game/quest.c
+++ src/game/quest.c
@@ -1,7 +1,7 @@
-#include "game/lib/quest.h"
+#include "game/quest.h"
 
-#include "game/lib/message.h"
-#include "game/lib/stat.h"
+#include "game/mes.h"
+#include "game/stat.h"
 
 #define MAX_QUEST 1000
 
@@ -13,20 +13,20 @@ typedef struct Quest {
     int dumb_dialog[QUEST_STATE_COUNT];
     char* normal_description;
     char* dumb_description;
-};
+} Quest;
 
 static_assert(sizeof(Quest) == 0x64, "wrong size");
 
 static bool quest_parse(const char* path, int start, int end);
 
 // 0x5B6E34
-static int quest_log_msg_file = -1;
+static mes_file_handle_t quest_log_mes_file = MES_FILE_HANDLE_INVALID;
 
 // 0x5B6E38
-static int quest_log_dumb_msg_file = -1;
+static mes_file_handle_t quest_log_dumb_mes_file = MES_FILE_HANDLE_INVALID;
 
 // 0x5FF414
-static int quest_xp_msg_file;
+static mes_file_handle_t quest_xp_mes_file;
 
 // 0x5FF418
 static int* quest_states;
@@ -35,14 +35,16 @@ static int* quest_states;
 static Quest* quests;
 
 // 0x4C45C0
-bool quest_init(GameContext* ctx)
+bool quest_init(GameContext* init_info)
 {
-    quest_states = (int*)calloc(MAX_QUEST, sizeof(*quest_states));
-    quests = (Quest*)calloc(MAX_QUEST, sizeof(*quests));
+    (void)init_info;
 
-    if (!message_load("Rules\\xp_quest.mes", &quest_xp_msg_file)) {
-        free(quest_states);
-        free(quests);
+    quest_states = (int*)CALLOC(MAX_QUEST, sizeof(*quest_states));
+    quests = (Quest*)CALLOC(MAX_QUEST, sizeof(*quests));
+
+    if (!mes_load("Rules\\xp_quest.mes", &quest_xp_mes_file)) {
+        FREE(quest_states);
+        FREE(quests);
         return false;
     }
 
@@ -52,7 +54,9 @@ bool quest_init(GameContext* ctx)
 // 0x4C4620
 void quest_reset()
 {
-    for (int index = 0; index < MAX_QUEST; index++) {
+    int index;
+
+    for (index = 0; index < MAX_QUEST; index++) {
         quest_states[index] = QUEST_STATE_ACCEPTED;
     }
 }
@@ -60,19 +64,24 @@ void quest_reset()
 // 0x4C4640
 void quest_exit()
 {
-    message_unload(quest_xp_msg_file);
-    free(quest_states);
-    free(quests);
+    mes_unload(quest_xp_mes_file);
+    FREE(quest_states);
+    FREE(quests);
 }
 
 // 0x4C4670
 bool quest_mod_load()
 {
-    for (int index = 0; index < MAX_QUEST; index++) {
-        for (int k = 0; k < QUEST_STATE_COUNT; k++) {
-            quests[index].normal_dialog[k] = -1;
-            quests[index].bad_reaction_dialog[k] = -1;
-            quests[index].dumb_dialog[k] = -1;
+    int index;
+    int state;
+    MesFileEntry mes_file_entry;
+    int num;
+
+    for (index = 0; index < MAX_QUEST; index++) {
+        for (state = 0; state < QUEST_STATE_COUNT; state++) {
+            quests[index].normal_dialog[state] = -1;
+            quests[index].bad_reaction_dialog[state] = -1;
+            quests[index].dumb_dialog[state] = -1;
         }
 
         quests[index].experience_level = 1;
@@ -81,25 +90,25 @@ bool quest_mod_load()
     }
 
     if (quest_parse("rules\\gamequest.mes", 1000, 1999)) {
-        MessageListItem message_list_item;
-
-        if (message_load("mes\\gamequestlog.mes", &quest_log_msg_file)) {
-            for (int id = 1000; id < 2000; id++) {
-                message_list_item.num = id;
-                if (message_find(quest_log_msg_file, &message_list_item)) {
-                    quests[id - 1000].normal_description = message_list_item.text;
+        if (mes_load("mes\\gamequestlog.mes", &quest_log_mes_file)) {
+            for (num = 1000; num < 2000; num++) {
+                mes_file_entry.num = num;
+                if (mes_search(quest_log_mes_file, &mes_file_entry)) {
+                    quests[num - 1000].normal_description = mes_file_entry.str;
                 }
             }
         }
 
-        if (message_load("mes\\gamequestlogdumb.mes", &quest_log_dumb_msg_file)) {
-            for (int id = 1000; id < 2000; id++) {
-                message_list_item.num = id;
-                if (message_find(quest_log_msg_file, &message_list_item)) {
-                    quests[id - 1000].dumb_description = message_list_item.text;
+        if (mes_load("mes\\gamequestlogdumb.mes", &quest_log_dumb_mes_file)) {
+            for (num = 1000; num < 2000; num++) {
+                mes_file_entry.num = num;
+                if (mes_search(quest_log_mes_file, &mes_file_entry)) {
+                    quests[num - 1000].dumb_description = mes_file_entry.str;
                 }
             }
         }
+
+        quest_reset();
     }
 
     return true;
@@ -108,21 +117,23 @@ bool quest_mod_load()
 // 0x4C47C0
 void quest_mod_unload()
 {
-    if (quest_log_msg_file != -1) {
-        message_unload(quest_log_msg_file);
-        quest_log_msg_file = -1;
+    if (quest_log_mes_file != -1) {
+        mes_unload(quest_log_mes_file);
+        quest_log_mes_file = -1;
     }
 
-    if (quest_log_dumb_msg_file != -1) {
-        message_unload(quest_log_dumb_msg_file);
-        quest_log_dumb_msg_file = -1;
+    if (quest_log_dumb_mes_file != -1) {
+        mes_unload(quest_log_dumb_mes_file);
+        quest_log_dumb_mes_file = -1;
     }
 }
 
 // 0x4C4800
 bool quest_load(LoadContext* ctx)
 {
-    if (tig_file_fread(quest_states, sizeof(*quest_states) * MAX_QUEST, 1, ctx->stream) != 1) return false;
+    if (tig_file_fread(quest_states, sizeof(*quest_states) * MAX_QUEST, 1, ctx->stream) != 1) {
+        return false;
+    }
 
     return true;
 }
@@ -130,7 +141,9 @@ bool quest_load(LoadContext* ctx)
 // 0x4C4830
 bool quest_save(TigFile* stream)
 {
-    if (tig_file_fwrite(quest_states, sizeof(*quest_states) * MAX_QUEST, 1, stream) != 1) return false;
+    if (tig_file_fwrite(quest_states, sizeof(*quest_states) * MAX_QUEST, 1, stream) != 1) {
+        return false;
+    }
 
     return true;
 }
@@ -138,60 +151,62 @@ bool quest_save(TigFile* stream)
 // 0x4C4860
 bool quest_parse(const char* path, int start, int end)
 {
-    int msg_file;
-    if (!message_load(path, &msg_file)) {
+    mes_file_handle_t mes_file;
+    MesFileEntry mes_file_entry;
+    int num;
+    char temp[2000];
+
+    if (!mes_load(path, &mes_file)) {
         return false;
     }
 
-    for (int id = start; id <= end; id++) {
-        MessageListItem message_list_item;
-        message_list_item.num = id;
+    for (num = start; num <= end; num++) {
+        mes_file_entry.num = num;
 
-        if (message_find(msg_file, &message_list_item)) {
-            char temp[2000];
-            strcpy(temp, message_list_item.text);
+        if (mes_search(mes_file, &mes_file_entry)) {
+            strcpy(temp, mes_file_entry.str);
 
-            quests[id - 1000].experience_level = atoi(strtok(temp, " "));
-            quests[id - 1000].alignment_adjustment = atoi(strtok(NULL, " "));
-            quests[id - 1000].normal_dialog[0] = atoi(strtok(NULL, " "));
-            quests[id - 1000].normal_dialog[1] = atoi(strtok(NULL, " "));
-            quests[id - 1000].normal_dialog[2] = atoi(strtok(NULL, " "));
-            quests[id - 1000].normal_dialog[3] = atoi(strtok(NULL, " "));
-            quests[id - 1000].normal_dialog[4] = atoi(strtok(NULL, " "));
-            quests[id - 1000].normal_dialog[5] = atoi(strtok(NULL, " "));
-            quests[id - 1000].normal_dialog[6] = atoi(strtok(NULL, " "));
-            quests[id - 1000].bad_reaction_dialog[0] = atoi(strtok(NULL, " "));
-            quests[id - 1000].bad_reaction_dialog[1] = atoi(strtok(NULL, " "));
-            quests[id - 1000].bad_reaction_dialog[2] = atoi(strtok(NULL, " "));
-            quests[id - 1000].bad_reaction_dialog[3] = atoi(strtok(NULL, " "));
-            quests[id - 1000].bad_reaction_dialog[4] = atoi(strtok(NULL, " "));
-            quests[id - 1000].bad_reaction_dialog[5] = atoi(strtok(NULL, " "));
-            quests[id - 1000].bad_reaction_dialog[6] = atoi(strtok(NULL, " "));
-            quests[id - 1000].dumb_dialog[0] = atoi(strtok(NULL, " "));
-            quests[id - 1000].dumb_dialog[1] = atoi(strtok(NULL, " "));
-            quests[id - 1000].dumb_dialog[2] = atoi(strtok(NULL, " "));
-            quests[id - 1000].dumb_dialog[3] = atoi(strtok(NULL, " "));
-            quests[id - 1000].dumb_dialog[4] = atoi(strtok(NULL, " "));
-            quests[id - 1000].dumb_dialog[5] = atoi(strtok(NULL, " "));
-            quests[id - 1000].dumb_dialog[6] = atoi(strtok(NULL, " "));
+            quests[num - 1000].experience_level = atoi(strtok(temp, " "));
+            quests[num - 1000].alignment_adjustment = atoi(strtok(NULL, " "));
+            quests[num - 1000].normal_dialog[QUEST_STATE_UNKNOWN] = atoi(strtok(NULL, " "));
+            quests[num - 1000].normal_dialog[QUEST_STATE_MENTIONED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].normal_dialog[QUEST_STATE_ACCEPTED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].normal_dialog[QUEST_STATE_ACHIEVED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].normal_dialog[QUEST_STATE_COMPLETED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].normal_dialog[QUEST_STATE_OTHER_COMPLETED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].normal_dialog[QUEST_STATE_BOTCHED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].bad_reaction_dialog[QUEST_STATE_UNKNOWN] = atoi(strtok(NULL, " "));
+            quests[num - 1000].bad_reaction_dialog[QUEST_STATE_MENTIONED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].bad_reaction_dialog[QUEST_STATE_ACCEPTED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].bad_reaction_dialog[QUEST_STATE_ACHIEVED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].bad_reaction_dialog[QUEST_STATE_COMPLETED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].bad_reaction_dialog[QUEST_STATE_OTHER_COMPLETED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].bad_reaction_dialog[QUEST_STATE_BOTCHED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].dumb_dialog[QUEST_STATE_UNKNOWN] = atoi(strtok(NULL, " "));
+            quests[num - 1000].dumb_dialog[QUEST_STATE_MENTIONED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].dumb_dialog[QUEST_STATE_ACCEPTED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].dumb_dialog[QUEST_STATE_ACHIEVED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].dumb_dialog[QUEST_STATE_COMPLETED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].dumb_dialog[QUEST_STATE_OTHER_COMPLETED] = atoi(strtok(NULL, " "));
+            quests[num - 1000].dumb_dialog[QUEST_STATE_BOTCHED] = atoi(strtok(NULL, " "));
         }
     }
 
-    message_unload(msg_file);
+    mes_unload(mes_file);
 
     return true;
 }
 
 // 0x4C51A0
-int quest_get_state(int id)
+int quest_get_state(int num)
 {
-    return quest_states[id - 1000];
+    return quest_states[num - 1000];
 }
 
 // 0x4C51C0
-int quest_set_state(int id, int state)
+int quest_set_state(int num, int state)
 {
-    int old_state = quest_states[id - 1000];
+    int old_state = quest_states[num - 1000];
     if (old_state == QUEST_STATE_COMPLETED || old_state == QUEST_STATE_BOTCHED) {
         return old_state;
     }
@@ -201,7 +216,7 @@ int quest_set_state(int id, int state)
     }
 
     if (state == QUEST_STATE_COMPLETED || state == QUEST_STATE_BOTCHED) {
-        quest_states[id - 1000] = state;
+        quest_states[num - 1000] = state;
         return state;
     } else {
         return QUEST_STATE_ACCEPTED;
@@ -209,22 +224,23 @@ int quest_set_state(int id, int state)
 }
 
 // 0x4C5250
-void quest_copy_description(object_id_t object_id, int quest_id, char* buffer)
+void quest_copy_description(object_id_t obj, int num, char* buffer)
 {
-    if (quests[quest_id - 1000].dumb_description != NULL && sub_4B0490(object_id, STAT_INTELLIGENCE) <= LOW_INTELLIGENCE) {
-        strcpy(buffer, quests[quest_id - 1000].dumb_description);
-    } else if (quests[quest_id - 1000].normal_description != NULL) {
-        strcpy(buffer, quests[quest_id - 1000].normal_description);
+    if (quests[num - 1000].dumb_description != NULL && sub_4B0490(obj, STAT_INTELLIGENCE) <= LOW_INTELLIGENCE) {
+        strcpy(buffer, quests[num - 1000].dumb_description);
+    } else if (quests[num - 1000].normal_description != NULL) {
+        strcpy(buffer, quests[num - 1000].normal_description);
     }
 }
 
 // 0x4C53C0
 int quest_get_xp(int xp_level)
 {
-    MessageListItem message_list_item;
-    message_list_item.num = xp_level;
-    if (message_find(quest_xp_msg_file, &message_list_item)) {
-        return atoi(message_list_item.text);
+    MesFileEntry mes_file_entry;
+
+    mes_file_entry.num = xp_level;
+    if (mes_search(quest_xp_mes_file, &mes_file_entry)) {
+        return atoi(mes_file_entry.str);
     } else {
         return 0;
     }
--- src/game/quest.h
+++ src/game/quest.h
@@ -1,8 +1,8 @@
-#ifndef ARCANUM_GAME_LIB_QUEST_H_
-#define ARCANUM_GAME_LIB_QUEST_H_
+#ifndef ARCANUM_GAME_QUEST_H_
+#define ARCANUM_GAME_QUEST_H_
 
 #include "game/context.h"
-#include "game/lib/object.h"
+#include "game/obj.h"
 
 typedef enum QuestState {
     QUEST_STATE_UNKNOWN,
@@ -13,18 +13,18 @@ typedef enum QuestState {
     QUEST_STATE_OTHER_COMPLETED,
     QUEST_STATE_BOTCHED,
     QUEST_STATE_COUNT,
-};
+} QuestState;
 
-bool quest_init(GameContext* ctx);
+bool quest_init(GameInitInfo* init_info);
 void quest_reset();
 void quest_exit();
 bool quest_mod_load();
 void quest_mod_unload();
-bool quest_load(LoadContext* ctx);
+bool quest_load(GameLoadInfo* load_info);
 bool quest_save(TigFile* stream);
 int quest_get_state(int id);
 int quest_set_state(int id, int state);
 void quest_copy_description(object_id_t object_id, int quest_id, char* buffer);
 int quest_get_xp(int xp_level);
 
-#endif /* ARCANUM_GAME_LIB_QUEST_H_ */
+#endif /* ARCANUM_GAME_QUEST_H_ */
