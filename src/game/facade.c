724c3857c5bd593a5d6b6a48fe3bd4317cb7c423
Alexander Batalov <alex.batalov@gmail.com>
2024-09-01 09:38:52 +0300

    Stub facade

---
+++ src/game/facade.c
@@ -0,0 +1,61 @@
+#include "game/facade.h"
+
+// 0x4C9DA0
+void facade_init()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9DE0
+void facade_exit()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9DF0
+void facade_resize()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9E00
+void sub_4C9E00()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9E70
+void sub_4C9E70()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4CA0F0
+void sub_4CA0F0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4CA240
+void sub_4CA240()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4CA2C0
+void sub_4CA2C0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4CA6B0
+void sub_4CA6B0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4CA7C0
+void sub_4CA7C0()
+{
+    // TODO: Incomplete.
+}
---
+++ src/game/facade.h
@@ -0,0 +1,6 @@
+#ifndef ARCANUM_GAME_FACADE_H_
+#define ARCANUM_GAME_FACADE_H_
+
+#include "game/context.h"
+
+#endif /* ARCANUM_GAME_FACADE_H_ */
