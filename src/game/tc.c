8c3b1e8658c073e1c8dca5dff27a046870d754e6
Alexander Batalov <alex.batalov@gmail.com>
2024-09-01 08:10:24 +0300

    Stub tc

---
+++ src/game/tc.c
@@ -0,0 +1,73 @@
+#include "game/tc.h"
+
+// 0x4C9280
+void tc_init()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9540
+void tc_exit()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C95A0
+void tc_resize()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C95F0
+void sub_4C95F0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9620
+void sub_4C9620()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C96C0
+void sub_4C96C0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C96F0
+void sub_4C96F0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9720
+void sub_4C9720()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9810
+void sub_4C9810()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9A10
+void sub_4C9A10()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9B90
+void sub_4C9B90()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4C9BE0
+void sub_4C9BE0()
+{
+    // TODO: Incomplete.
+}
---
+++ src/game/tc.h
@@ -0,0 +1,6 @@
+#ifndef ARCANUM_GAME_TC_H_
+#define ARCANUM_GAME_TC_H_
+
+#include "game/context.h"
+
+#endif /* ARCANUM_GAME_TC_H_ */
