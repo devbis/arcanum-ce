89c0e8f29406b78e18ff12652bab55fbe09e316b
Alexander Batalov <alex.batalov@gmail.com>
2024-09-07 13:01:40 +0300

    Add item ui

---
+++ src/ui/item_ui.c
@@ -0,0 +1,46 @@
+#include "ui/item_ui.h"
+
+#include "game/obj.h"
+#include "ui/intgame.h"
+
+// 0x6810D8
+static int64_t qword_6810D8;
+
+// 0x5719D0
+bool item_ui_init(GameInitInfo* init_info)
+{
+    (void)init_info;
+
+    return true;
+}
+
+// 0x5719E0
+void item_ui_exit()
+{
+}
+
+// 0x5719F0
+void sub_5719F0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x571AA0
+void sub_571AA0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x571C80
+void sub_571C80()
+{
+    sub_551A80(0);
+    qword_6810D8 = OBJ_HANDLE_NULL;
+    sub_553A60(21);
+}
+
+// 0x571CB0
+void sub_571CB0()
+{
+    // TODO: Incomplete.
+}
---
+++ src/ui/item_ui.h
@@ -0,0 +1,10 @@
+#ifndef ARCANUM_UI_ITEM_UI_H_
+#define ARCANUM_UI_ITEM_UI_H_
+
+#include "game/context.h"
+
+bool item_ui_init(GameInitInfo* init_info);
+void item_ui_exit();
+void sub_571C80();
+
+#endif /* ARCANUM_UI_ITEM_UI_H_ */
