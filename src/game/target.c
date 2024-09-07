ed3f70c8292c8a8d78e792c0c4951fda2d5c4ab2
Alexander Batalov <alex.batalov@gmail.com>
2024-09-07 14:25:21 +0300

    Stub target

---
+++ src/game/target.c
@@ -0,0 +1,199 @@
+#include "game/target.h"
+
+// 0x4F24F0
+void target_init()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2560
+void target_exit()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2570
+void target_reset()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2580
+void target_resize()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F25B0
+void sub_4F25B0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F25D0
+void sub_4F25D0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F25E0
+void sub_4F25E0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2600
+void sub_4F2600()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2680
+void sub_4F2680()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F27F0
+void sub_4F27F0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2810
+void sub_4F2810()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2830
+void sub_4F2830()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F28A0
+void sub_4F28A0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2C60
+void sub_4F2C60()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2CB0
+void sub_4F2CB0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2D10
+void sub_4F2D10()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F2D20
+void sub_4F2D20()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F3F10
+void sub_4F3F10()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F3FD0
+void sub_4F3FD0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F4050
+void sub_4F4050()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F40B0
+void sub_4F40B0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F4E40
+void sub_4F4E40()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F5090
+void sub_4F5090()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F50C0
+void sub_4F50C0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F51B0
+void sub_4F51B0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F5270
+void sub_4F5270()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F52D0
+void sub_4F52D0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F5320
+void sub_4F5320()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F5330
+void sub_4F5330()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F5590
+void sub_4F5590()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F5690
+void sub_4F5690()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F5770
+void sub_4F5770()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4F57C0
+void sub_4F57C0()
+{
+    // TODO: Incomplete.
+}
---
+++ src/game/target.h
@@ -0,0 +1,6 @@
+#ifndef ARCANUM_GAME_TARGET_H_
+#define ARCANUM_GAME_TARGET_H_
+
+#include "game/context.h"
+
+#endif /* ARCANUM_GAME_TARGET_H_ */
