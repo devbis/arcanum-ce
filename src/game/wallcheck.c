d82c89777b165ef71a483b571600cba33a11c49f
Alexander Batalov <alex.batalov@gmail.com>
2024-09-07 16:28:11 +0300

    Stub wallcheck

---
+++ src/game/wallcheck.c
@@ -0,0 +1,91 @@
+#include "game/wallcheck.h"
+
+// 0x437D90
+void wallcheck_init()
+{
+    // TODO: Incomplete.
+}
+
+// 0x437DB0
+void wallcheck_reset()
+{
+    // TODO: Incomplete.
+}
+
+// 0x437E00
+void wallcheck_exit()
+{
+    // TODO: Incomplete.
+}
+
+// 0x437E10
+void sub_437E10()
+{
+    // TODO: Incomplete.
+}
+
+// 0x437E50
+void sub_437E50()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4384F0
+void wallcheck_flush()
+{
+    // TODO: Incomplete.
+}
+
+// 0x438500
+void sub_438500()
+{
+    // TODO: Incomplete.
+}
+
+// 0x438520
+void sub_438520()
+{
+    // TODO: Incomplete.
+}
+
+// 0x438530
+void sub_438530()
+{
+    // TODO: Incomplete.
+}
+
+// 0x438570
+void sub_438570()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4386B0
+void sub_4386B0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x438720
+void sub_438720()
+{
+    // TODO: Incomplete.
+}
+
+// 0x4387C0
+void sub_4387C0()
+{
+    // TODO: Incomplete.
+}
+
+// 0x438830
+void sub_438830()
+{
+    // TODO: Incomplete.
+}
+
+// 0x438A50
+void wallcheck_roof_faded_clear()
+{
+    // TODO: Incomplete.
+}
---
+++ src/game/wallcheck.h
@@ -0,0 +1,6 @@
+#ifndef ARCANUM_GAME_WALLCHECK_H_
+#define ARCANUM_GAME_WALLCHECK_H_
+
+#include "game/context.h"
+
+#endif /* ARCANUM_GAME_WALLCHECK_H_ */
