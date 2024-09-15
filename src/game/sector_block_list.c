53028c2f3fbcd7125a1caf55d6f2916fe06a95e5
Alexander Batalov <alex.batalov@gmail.com>
2024-09-15 13:50:49 +0300

    Add sector block list

---
+++ src/game/sector_block_list.c
@@ -0,0 +1,141 @@
+#include "game/sector_block_list.h"
+
+static void sub_4F83A0();
+static void sub_4F83D0();
+
+// 0x603DE4
+static SectorBlockListNode* sector_block_list_head;
+
+// 0x4F81E0
+bool sector_block_list_init(SectorBlockList* list)
+{
+    memset(list, 0, sizeof(*list));
+
+    return true;
+}
+
+// 0x4F8200
+bool sector_block_list_reset(SectorBlockList* list)
+{
+    memset(list, 0, sizeof(*list));
+
+    return true;
+}
+
+// 0x4F8230
+bool sector_block_list_exit(SectorBlockList* list)
+{
+    memset(list, 0, sizeof(*list));
+
+    return true;
+}
+
+// 0x4F8250
+bool sector_block_list_load(SectorBlockList* list, TigFile* stream)
+{
+    if (tig_file_fread(list->field_4, sizeof(*list->field_4), 128, stream) != 128) {
+        return false;
+    }
+
+    list->field_0 = 0;
+
+    return true;
+}
+
+// 0x4F8290
+bool sector_block_list_save(SectorBlockList* list, TigFile* stream)
+{
+    if (tig_file_fwrite(list->field_4, sizeof(*list->field_4), 128, stream) != 128) {
+        return false;
+    }
+
+    list->field_0 = 0;
+
+    return true;
+}
+
+// 0x4F82D0
+void sector_block_list_load_with_dif(SectorBlockList* list, TigFile* stream)
+{
+    if (!sector_block_list_load(list, stream)) {
+        return false;
+    }
+
+    list->field_0 = 1;
+
+    return true;
+}
+
+// 0x4F8300
+void sector_block_list_save_with_dif(SectorBlockList* list, TigFile* stream)
+{
+    if (!sector_block_list_save(list, stream)) {
+        return false;
+    }
+
+    list->field_0 = 1;
+
+    return true;
+}
+
+// 0x4F8330
+void sub_4F8330()
+{
+}
+
+// 0x4F8340
+void sub_4F8340()
+{
+    sub_4F83D0();
+}
+
+// 0x4F8350
+SectorBlockListNode* sector_block_list_node_create()
+{
+    SectorBlockListNode* node;
+
+    if (sector_block_list_head == NULL) {
+        sub_4F83A0();
+    }
+
+    node = sector_block_list_head;
+    sector_block_list_head = node->next;
+
+    node->next = NULL;
+
+    return node;
+}
+
+// 0x4F8380
+void sector_block_list_node_destroy(SectorBlockListNode* node)
+{
+    node->next = sector_block_list_head;
+    sector_block_list_head = node;
+}
+
+// 0x4F83A0
+void sub_4F83A0()
+{
+    int index;
+    SectorBlockListNode* node;
+
+    if (sector_block_list_head == NULL) {
+        for (index = 0; index < 64; index++) {
+            node = (SectorBlockListNode*)MALLOC(sizeof(*node));
+            node->next = sector_block_list_head;
+            sector_block_list_head = node;
+        }
+    }
+}
+
+// 0x4F83D0
+void sub_4F83D0()
+{
+    SectorBlockListNode* next;
+
+    while (sector_block_list_head != NULL) {
+        next = sector_block_list_head->next;
+        FREE(sector_block_list_head);
+        sector_block_list_head = next;
+    }
+}
---
+++ src/game/sector_block_list.h
@@ -0,0 +1,34 @@
+#ifndef ARCANUM_GAME_SECTOR_BLOCK_LIST_H_
+#define ARCANUM_GAME_SECTOR_BLOCK_LIST_H_
+
+#include <tig/tig.h>
+
+typedef struct SectorBlockList {
+    /* 0000 */ int field_0;
+    /* 0004 */ int field_4[128];
+} SectorBlockList;
+
+// See 0x4F81E0.
+static_assert(sizeof(SectorBlockList) == 0x204, "wrong size");
+
+typedef struct SectorBlockListNode {
+    /* 0000 */ int field_0;
+    /* 0004 */ struct SectorBlockListNode* next;
+} SectorBlockListNode;
+
+// See 0x4F83B1.
+static_assert(sizeof(SectorBlockListNode) == 0x8, "wrong size");
+
+bool sector_block_list_init(SectorBlockList* list);
+bool sector_block_list_reset(SectorBlockList* list);
+bool sector_block_list_exit(SectorBlockList* list);
+bool sector_block_list_load(SectorBlockList* list, TigFile* stream);
+bool sector_block_list_save(SectorBlockList* list, TigFile* stream);
+void sector_block_list_load_with_dif(SectorBlockList* list, TigFile* stream);
+void sector_block_list_save_with_dif(SectorBlockList* list, TigFile* stream);
+void sub_4F8330();
+void sub_4F8340();
+SectorBlockListNode* sector_block_list_node_create();
+void sector_block_list_node_destroy(SectorBlockListNode* node);
+
+#endif /* ARCANUM_GAME_SECTOR_BLOCK_LIST_H_ */
