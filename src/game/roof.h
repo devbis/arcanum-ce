74fe10b3db36342a73aa2bce5d7306a21a38d7a7
Alexander Batalov <alex.batalov@gmail.com>
2024-09-15 08:27:39 +0300

    Expose some roof functions

--- src/game/roof.h
+++ src/game/roof.h
@@ -7,5 +7,7 @@ bool roof_init(GameInitInfo* init_info);
 void roof_exit();
 void roof_resize(ResizeInfo* resize_info);
 bool sub_439100(ViewOptions* view_options);
+void sub_43A110(int a1);
+int sub_43A130();
 
 #endif /* ARCANUM_GAME_ROOF_H_ */
