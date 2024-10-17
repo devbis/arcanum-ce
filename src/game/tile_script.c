c175b20251d093c8b593a4281045b5c3e93f65f9
Alexander Batalov <alex.batalov@gmail.com>
2024-10-17 21:41:48 +0300

    Complete tile script

--- src/game/tile_script.c
+++ src/game/tile_script.c
@@ -1,5 +1,21 @@
 #include "game/tile_script.h"
 
+#include "game/sector.h"
+#include "game/tile_script_list.h"
+#include "game/tile.h"
+
+typedef struct TileScript {
+    int64_t loc;
+    Script scr;
+} TileScript;
+
+static_assert(sizeof(TileScript) == 0x18, "wrong size");
+
+static bool sub_4C07B0(int64_t loc, TileScript* tile_script);
+static bool sub_4C0830(TileScript* tile_script);
+static bool sub_4C0930(int64_t loc);
+static void sub_4C0A40(int64_t sector_id, int tile, TigRect* rect);
+
 // 0x5FC868
 static tig_art_id_t dword_5FC868;
 
@@ -7,13 +23,13 @@ static tig_art_id_t dword_5FC868;
 static bool tile_script_editor;
 
 // 0x5FC870
-static GameContextF8* dword_5FC870;
+static GameContextF8* tile_script_iso_invalidate_rect;
 
 // 0x5FC874
 static bool tile_script_initialized;
 
 // 0x5FC878
-static int dword_5FC878;
+static bool dword_5FC878;
 
 // 0x5FC87C
 static tig_window_handle_t tile_script_iso_window_handle;
@@ -28,7 +44,7 @@ static tig_art_id_t dword_5FC888;
 bool tile_script_init(GameInitInfo* init_info)
 {
     tile_script_iso_window_handle = init_info->iso_window_handle;
-    dword_5FC870 = init_info->field_8;
+    tile_script_iso_invalidate_rect = init_info->field_8;
     tile_script_editor = init_info->editor;
     tile_script_view_options.type = VIEW_TYPE_ISOMETRIC;
 
@@ -36,7 +52,7 @@ bool tile_script_init(GameInitInfo* init_info)
     tig_art_interface_id_create(476, 0, 0, 0, &dword_5FC888);
 
     tile_script_initialized = true;
-    dword_5FC878 = 1;
+    dword_5FC878 = true;
 
     sub_4F6310();
 
@@ -51,17 +67,15 @@ void tile_script_reset()
 // 0x4C05C0
 void tile_script_exit()
 {
-    void* next;
+    TileScriptListNode* next;
 
     tile_script_initialized = false;
 
-    while (off_603DD0 != NULL) {
-        next = curr->field_14;
-        FREE(off_603DD0);
-        off_603DD0 = next;
+    while (tile_script_node_head != NULL) {
+        next = tile_script_node_head->next;
+        FREE(tile_script_node_head);
+        tile_script_node_head = next;
     }
-
-    off_603DD0 = NULL;
 }
 
 // 0x4C05D0
@@ -76,4 +90,210 @@ bool sub_4C05E0(ViewOptions* view_options)
     tile_script_view_options = *view_options;
 
     return true;
+}
+
+// 0x4C0630
+void sub_4C0630(UnknownContext* a1)
+{
+    TigArtBlitInfo art_blit_info;
+    TigArtFrameData art_frame_data;
+    TigRect src_rect;
+    TigRect dst_rect;
+    Sector601808* sector_node;
+    Sector* sector;
+    TileScriptListNode* tile_script_node;
+    TigRect tile_rect;
+    TigRectListNode* rect_node;
+
+    if (!dword_5FC878) {
+        return;
+    }
+
+    switch (tile_script_view_options.type) {
+    case VIEW_TYPE_ISOMETRIC:
+        art_blit_info.art_id = dword_5FC868;
+        break;
+    case VIEW_TYPE_TOP_DOWN:
+        art_blit_info.art_id = dword_5FC888;
+        tig_art_frame_data(art_blit_info.art_id, &art_frame_data);
+        src_rect.x = 0;
+        src_rect.y = 0;
+        src_rect.width = art_frame_data.width;
+        src_rect.height = art_frame_data.height;
+        break;
+    default:
+        // Unknown view type.
+        return;
+    }
+
+    art_blit_info.flags = 0;
+    art_blit_info.src_rect = &src_rect;
+    art_blit_info.dst_rect = &dst_rect;
+
+    sector_node = a1->field_C;
+    while (sector_node != NULL) {
+        if (sector_lock(sector_node->id, &sector)) {
+            tile_script_node = sector->tile_scripts.head;
+            while (tile_script_node != NULL) {
+                sub_4C0A40(sector_node->id, tile_script_node->id, &tile_rect);
+                rect_node = *a1->rects;
+                while (rect_node != NULL) {
+                    if (tig_rect_intersection(&tile_rect, &(rect_node->rect), &dst_rect) == TIG_OK) {
+                        src_rect.x = dst_rect.x - tile_rect.x;
+                        src_rect.y = dst_rect.y - tile_rect.y;
+                        src_rect.width = dst_rect.width;
+                        src_rect.height = dst_rect.height;
+                    }
+                    tig_window_blit_art(tile_script_iso_window_handle, &art_blit_info);
+                    rect_node = rect_node->next;
+                }
+                tile_script_node = tile_script_node->next;
+            }
+            sector_unlock(sector_node->id);
+        }
+        sector_node = sector_node->next;
+    }
+}
+
+// 0x4C07B0
+bool sub_4C07B0(int64_t loc, TileScript* tile_script)
+{
+    int64_t sector_id;
+    Sector* sector;
+    int tile;
+    bool rc;
+
+    sector_id = sub_4CFC50(loc);
+    if (!sector_lock(sector_id, &sector)) {
+        return false;
+    }
+
+    tile = sub_4D7090(loc);
+    rc = sub_4F6570(&(sector->tile_scripts), tile, &(tile_script->scr));
+    tile_script->loc = loc;
+    sector_unlock(sector_id);
+
+    return rc;
+}
+
+// 0x4C0830
+bool sub_4C0830(TileScript* tile_script)
+{
+    int tile;
+    int sector_id;
+    Sector* sector;
+    TigRect dirty_rect;
+
+    tile = sub_4D7090(tile_script->loc);
+    sector_id = sub_4CFC50(tile_script->loc);
+    if (!sector_lock(sector_id, &sector)) {
+        return false;
+    }
+
+    if (!tile_script_editor || tile_script->scr.num != 0) {
+        sub_4F6470(&(sector->tile_scripts), tile, &(tile_script->scr));
+    } else {
+        sub_4F6520(&(sector->tile_scripts), tile);
+    }
+
+    sector_unlock(sector_id);
+
+    sub_4C0A40(sector_id, tile, &dirty_rect);
+    tile_script_iso_invalidate_rect(&dirty_rect);
+}
+
+// 0x4C08E0
+void sub_4C08E0(int64_t old_loc, int64_t new_loc)
+{
+    TileScript tile_script;
+
+    if (sub_4C07B0(old_loc, &tile_script)) {
+        sub_4C0930(old_loc);
+        tile_script.loc = new_loc;
+        sub_4C0830(&tile_script);
+    }
+}
+
+// 0x4C0930
+bool sub_4C0930(int64_t loc)
+{
+    TileScript tile_script;
+
+    if (!sub_4C07B0(loc, &tile_script)) {
+        return false;
+    }
+
+    tile_script.scr.num = 0;
+    sub_4C0830(&tile_script);
+
+    return true;
+}
+
+// 0x4C0980
+void sub_4C0980(int64_t loc, int64_t triggerer_obj)
+{
+    TileScript tile_script;
+    Script scr;
+    ScriptInvocation invocation;
+
+    if (sub_4C07B0(loc, &tile_script)) {
+        scr = tile_script.scr;
+
+        invocation.script = &(tile_script.scr);
+        invocation.attachee_obj = OBJ_HANDLE_NULL;
+        invocation.extra_obj = OBJ_HANDLE_NULL;
+        invocation.line = 0;
+        invocation.field_20 = SAP_USE;
+        invocation.triggerer_obj = triggerer_obj;
+        sub_4449B0(&invocation);
+
+        if (invocation.script->num != scr.num
+            || invocation.script->hdr.flags != scr.hdr.flags
+            || invocation.script->hdr.counters != scr.hdr.counters) {
+            sub_4C0830(&tile_script);
+        }
+    }
+}
+
+// 0x4C0A40
+void sub_4C0A40(int64_t sector_id, int tile, TigRect* rect)
+{
+    int64_t loc;
+    int64_t x;
+    int64_t y;
+    TigArtFrameData art_frame_data;
+
+    loc = (tile >> 6) + ((sector_id >> 26) << 6);
+    loc |= (tile & 0x3F) + ((sector_id & 0x3FFFFFF) << 6);
+    sub_4B8680(loc, &x, &y);
+
+    if (x < INT_MIN || x > INT_MAX
+        || y < INT_MIN || y > INT_MAX) {
+        rect->x = 0;
+        rect->y = 0;
+        rect->width = 0;
+        rect->height = 0;
+        return;
+    }
+
+    if (tile_script_view_options.type == VIEW_TYPE_TOP_DOWN) {
+        rect->x = (int)x;
+        rect->y = (int)y;
+        rect->width = tile_script_view_options.zoom;
+        rect->height = tile_script_view_options.zoom;
+        return;
+    }
+
+    if (tig_art_frame_data(dword_5FC868, &art_frame_data) != TIG_OK) {
+        rect->x = 0;
+        rect->y = 0;
+        rect->width = 0;
+        rect->height = 0;
+        return;
+    }
+
+    rect->x = (int)x - art_frame_data.hot_x + 40;
+    rect->y = (int)y - (40 - art_frame_data.height) / 2 - art_frame_data.hot_y + 40;
+    rect->width = art_frame_data.width;
+    rect->height = art_frame_data.height;
 }
--- src/game/tile_script.h
+++ src/game/tile_script.h
@@ -8,5 +8,8 @@ void tile_script_reset();
 void tile_script_exit();
 void tile_script_resize(ResizeInfo* resize_info);
 bool sub_4C05E0(ViewOptions* view_options);
+void sub_4C0630(UnknownContext* a1);
+void sub_4C08E0(int64_t old_loc, int64_t new_loc);
+void sub_4C0980(int64_t loc, int64_t triggerer_obj);
 
 #endif /* ARCANUM_GAME_TILE_SCRIPT_H_ */
