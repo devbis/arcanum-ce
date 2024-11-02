cfae03f53c9404cf842effc193142c0c50de11ef
Alexander Batalov <alex.batalov@gmail.com>
2024-11-02 09:56:00 +0300

    Add sub_5719F0

--- src/ui/item_ui.c
+++ src/ui/item_ui.c
@@ -1,7 +1,22 @@
 #include "ui/item_ui.h"
 
+#include "game/magictech.h"
+#include "game/mt_item.h"
 #include "game/obj.h"
+#include "game/target.h"
+#include "game/trap.h"
 #include "ui/intgame.h"
+
+static void sub_5719F0(int64_t item_obj, uint64_t** tgt_ptr);
+
+// 0x5CAC40
+static uint64_t qword_5CAC40 = Tgt_Object;
+
+// 0x5CAC48
+static uint64_t qword_5CAC48 = Tgt_Obj_Inven;
+
+// 0x5CAC50
+static uint64_t qword_5CAC50 = Tgt_Tile | Tgt_Object;
 
 // 0x6810D8
 static int64_t qword_6810D8;
@@ -20,9 +35,28 @@ void item_ui_exit()
 }
 
 // 0x5719F0
-void sub_5719F0()
+void sub_5719F0(int64_t item_obj, uint64_t** tgt_ptr)
 {
-    // TODO: Incomplete.
+    int mana_store;
+    unsigned int flags;
+    int spl;
+
+    *tgt_ptr = 0;
+
+    if (item_obj != OBJ_HANDLE_NULL) {
+        mana_store = obj_field_int32_get(item_obj, OBJ_F_ITEM_SPELL_MANA_STORE);
+        flags = obj_field_int32_get(item_obj, OBJ_F_ITEM_FLAGS);
+        spl = mt_item_spell(item_obj, 0);
+        if (mana_store != 0 || (flags & OIF_IS_MAGICAL) != 0) {
+            sub_459F20(spl, tgt_ptr);
+        } else if (trap_is_trap_device(item_obj)) {
+            *tgt_ptr = &qword_5CAC50;
+        } else if (spl == -1) {
+            *tgt_ptr = &qword_5CAC40;
+        } else {
+            sub_459F20(spl, tgt_ptr);
+        }
+    }
 }
 
 // 0x571AA0
