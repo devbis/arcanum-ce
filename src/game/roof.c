1de492fb681f8db9206361f522f0e3ca4a4847cd
Alexander Batalov <alex.batalov@gmail.com>
2024-11-08 19:55:37 +0300

    Complete roof

--- src/game/roof.c
+++ src/game/roof.c
@@ -1,12 +1,104 @@
 #include "game/roof.h"
 
+#include "game/gamelib.h"
+#include "game/light.h"
+#include "game/location.h"
 #include "game/mes.h"
+#include "game/sector.h"
 #include "game/tile.h"
 
+static int sub_4395A0(int64_t loc);
+static int sub_4395C0(int a1);
+static void sub_439640(int64_t a1, int64_t* x, int64_t* y);
+static tig_art_id_t sub_4396A0(int64_t loc);
+static bool sub_439700(int64_t loc, tig_art_id_t aid);
 static void sub_43A140(int x, int y, tig_art_id_t aid, TigRect* rect);
+static void sub_43A1A0(int64_t loc, int a2, int a3);
 
 // 0x5A53A0
-static int dword_5A53A0 = 256;
+static unsigned int roof_blit_flags = TIG_ART_BLT_BLEND_ALPHA_CONST;
+
+// 0x5A53A4
+static char byte_5A53A4[13][4][4] = {
+    {
+        { 0, 0, 0, 0 },
+        { 0, 0, 0, 0 },
+        { 1, 1, 1, 0 },
+        { 1, 1, 1, 0 },
+    },
+    {
+        { 1, 1, 1, 0 },
+        { 1, 1, 1, 0 },
+        { 1, 1, 1, 0 },
+        { 1, 1, 1, 0 },
+    },
+    {
+        { 0, 0, 0, 0 },
+        { 0, 0, 0, 0 },
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+    },
+    {
+        { 1, 1, 1, 0 },
+        { 1, 1, 1, 0 },
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+    },
+    {
+        { 1, 1, 1, 0 },
+        { 1, 1, 1, 0 },
+        { 1, 1, 1, 0 },
+        { 0, 0, 0, 0 },
+    },
+    {
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 0 },
+    },
+    {
+        { 0, 0, 1, 1 },
+        { 0, 0, 1, 1 },
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+    },
+    {
+        { 0, 0, 0, 0 },
+        { 0, 0, 0, 0 },
+        { 0, 0, 1, 1 },
+        { 0, 0, 1, 1 },
+    },
+    {
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+    },
+    {
+        { 0, 0, 1, 1 },
+        { 0, 0, 1, 1 },
+        { 0, 0, 1, 1 },
+        { 0, 0, 0, 0 },
+    },
+    {
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+        { 0, 0, 0, 0 },
+    },
+    {
+        { 0, 0, 1, 1 },
+        { 0, 0, 1, 1 },
+        { 0, 0, 1, 1 },
+        { 0, 0, 1, 1 },
+    },
+    {
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+        { 1, 1, 1, 1 },
+        { 0, 0, 1, 1 },
+    },
+};
 
 // 0x5E2E30
 static uint8_t roof_partial_opacity;
@@ -76,7 +168,7 @@ bool roof_init(GameInitInfo* init_info)
 
     dword_5E2E38 = tig_video_3d_check_initialized() == TIG_OK;
     if (!dword_5E2E38) {
-        dword_5A53A0 = TIG_ART_BLT_BLEND_ALPHA_STIPPLE_D;
+        roof_blit_flags = TIG_ART_BLT_BLEND_ALPHA_STIPPLE_D;
     }
 
     return true;
@@ -102,10 +194,170 @@ bool sub_439100(ViewOptions* view_options)
     return true;
 }
 
-// 0x4395A0
-int sub_4395A0(int a1, int a2)
+// 0x439140
+void sub_439140(UnknownContext* info)
 {
-    return sub_4395C0(sub_4D7090(a1, a2));
+    TigArtBlitInfo art_blit_info;
+    TigRect dst_rect;
+    TigRect src_rect;
+    LocRect loc_rect;
+    int64_t y;
+    int64_t x;
+    unsigned int flags;
+    tig_art_id_t aid;
+    int64_t cur_x;
+    int64_t cur_y;
+    TigRect roof_rect;
+    TigRectListNode* node;
+
+    if (!dword_5E2E40) {
+        return;
+    }
+
+    if (roof_view_options.type != VIEW_TYPE_ISOMETRIC) {
+        return;
+    }
+
+    if (dword_5E2E38) {
+        art_blit_info.color = light_get_outdoor_color();
+        flags = TIG_ART_BLT_BLEND_COLOR_CONST;
+    } else {
+        flags = 0;
+    }
+
+    loc_rect = *info->field_4;
+
+    for (y = loc_rect.y1; y <= loc_rect.y2; y += 4) {
+        for (x = loc_rect.x1; x <= loc_rect.x2; x += 4) {
+            aid = sub_4396A0(x | y);
+            if (aid != TIG_ART_ID_INVALID
+                && !sub_5048D0(aid)) {
+                sub_439640(x | y, &cur_x, &cur_y);
+                if (cur_x > INT_MIN
+                    && cur_x < INT_MAX
+                    && cur_y > INT_MIN
+                    && cur_y < INT_MAX) {
+                    sub_43A140((int)x, (int)y, aid, &roof_rect);
+                    node = *info->rects;
+                    while (node != NULL) {
+                        if (tig_rect_intersection(&roof_rect, &node->rect, &dst_rect) == TIG_OK) {
+                            src_rect.x = dst_rect.x - roof_rect.x;
+                            src_rect.y = dst_rect.y - roof_rect.y;
+                            src_rect.width = dst_rect.width;
+                            src_rect.height = dst_rect.height;
+
+                            art_blit_info.art_id = aid;
+                            art_blit_info.src_rect = &src_rect;
+                            art_blit_info.dst_rect = &dst_rect;
+                            art_blit_info.flags = flags;
+
+                            if (sub_504940(aid)) {
+                                if (roof_blit_flags == TIG_ART_BLT_BLEND_ALPHA_CONST) {
+                                    art_blit_info.flags |= TIG_ART_BLT_BLEND_ALPHA_LERP_BOTH;
+
+                                    switch (sub_504840(aid)) {
+                                    case 0:
+                                        art_blit_info.alpha[0] = roof_full_transparency;
+                                        art_blit_info.alpha[1] = roof_partial_opacity;
+                                        art_blit_info.alpha[2] = roof_partial_opacity;
+                                        art_blit_info.alpha[3] = roof_full_transparency;
+                                        break;
+                                    case 1:
+                                        art_blit_info.alpha[0] = roof_partial_opacity;
+                                        art_blit_info.alpha[2] = roof_partial_opacity;
+                                        art_blit_info.alpha[1] = roof_full_opacity;
+                                        art_blit_info.alpha[3] = roof_full_transparency;
+                                        break;
+                                    case 2:
+                                        art_blit_info.alpha[0] = roof_full_transparency;
+                                        art_blit_info.alpha[1] = roof_partial_opacity;
+                                        art_blit_info.alpha[2] = roof_full_opacity;
+                                        art_blit_info.alpha[3] = roof_partial_opacity;
+                                        break;
+                                    case 3:
+                                        art_blit_info.alpha[0] = roof_partial_opacity;
+                                        art_blit_info.alpha[1] = roof_full_opacity;
+                                        art_blit_info.alpha[2] = roof_full_opacity;
+                                        art_blit_info.alpha[3] = roof_partial_opacity;
+                                        break;
+                                    case 4:
+                                        art_blit_info.alpha[0] = roof_partial_opacity;
+                                        art_blit_info.alpha[1] = roof_partial_opacity;
+                                        art_blit_info.alpha[2] = roof_full_transparency;
+                                        art_blit_info.alpha[3] = roof_full_transparency;
+                                        break;
+                                    case 5:
+                                        art_blit_info.alpha[0] = roof_full_opacity;
+                                        art_blit_info.alpha[1] = roof_full_opacity;
+                                        art_blit_info.alpha[2] = roof_partial_opacity;
+                                        art_blit_info.alpha[3] = roof_partial_opacity;
+                                        break;
+                                    case 6:
+                                        art_blit_info.alpha[0] = roof_partial_opacity;
+                                        art_blit_info.alpha[1] = roof_partial_opacity;
+                                        art_blit_info.alpha[2] = roof_full_opacity;
+                                        art_blit_info.alpha[3] = roof_full_opacity;
+                                        break;
+                                    case 7:
+                                        art_blit_info.alpha[0] = roof_full_transparency;
+                                        art_blit_info.alpha[1] = roof_full_transparency;
+                                        art_blit_info.alpha[2] = roof_partial_opacity;
+                                        art_blit_info.alpha[3] = roof_partial_opacity;
+                                        break;
+                                    case 8:
+                                        art_blit_info.alpha[0] = roof_full_opacity;
+                                        art_blit_info.alpha[1] = roof_full_opacity;
+                                        art_blit_info.alpha[2] = roof_full_opacity;
+                                        art_blit_info.alpha[3] = roof_full_opacity;
+                                        break;
+                                    case 9:
+                                        art_blit_info.alpha[0] = roof_partial_opacity;
+                                        art_blit_info.alpha[1] = roof_full_transparency;
+                                        art_blit_info.alpha[2] = roof_full_transparency;
+                                        art_blit_info.alpha[3] = roof_partial_opacity;
+                                        break;
+                                    case 10:
+                                        art_blit_info.alpha[0] = roof_full_opacity;
+                                        art_blit_info.alpha[1] = roof_partial_opacity;
+                                        art_blit_info.alpha[2] = roof_full_transparency;
+                                        art_blit_info.alpha[3] = roof_partial_opacity;
+                                        break;
+                                    case 11:
+                                        art_blit_info.alpha[0] = roof_partial_opacity;
+                                        art_blit_info.alpha[2] = roof_partial_opacity;
+                                        art_blit_info.alpha[1] = roof_full_transparency;
+                                        art_blit_info.alpha[3] = roof_full_opacity;
+                                        break;
+                                    case 12:
+                                        art_blit_info.alpha[0] = roof_full_opacity;
+                                        art_blit_info.alpha[1] = roof_partial_opacity;
+                                        art_blit_info.alpha[2] = roof_partial_opacity;
+                                        art_blit_info.alpha[3] = roof_full_opacity;
+                                        break;
+                                    default:
+                                        // Should be unreachable.
+                                        __assume(0);
+                                    }
+                                } else {
+                                    art_blit_info.flags |= roof_blit_flags;
+                                }
+                            }
+
+                            art_blit_info.scratch_video_buffer = dword_739E7C;
+                            tig_window_blit_art(roof_window_handle, &art_blit_info);
+                        }
+                        node = node->next;
+                    }
+                }
+            }
+        }
+    }
+}
+
+// 0x4395A0
+int sub_4395A0(int64_t loc)
+{
+    return sub_4395C0(sub_4D7090((int)(uint32_t)location_get_x(loc), (int)(uint32_t)location_get_y(loc)));
 }
 
 // 0x4395C0
@@ -121,6 +373,214 @@ int64_t sub_4395E0(int64_t a1)
         | (((a1 >> 32) - (a1 >> 32) % 4 + 2) << 32);
 }
 
+// 0x439640
+void sub_439640(int64_t a1, int64_t* x, int64_t* y)
+{
+    sub_4B8680(sub_4395E0(a1), x, y);
+
+    *x = location_make(location_get_x(*x) - 120, location_get_y(*x) - 120);
+    *y = location_make(location_get_y(*y) - 200, location_get_y(*y) - 120);
+}
+
+// 0x4396A0
+tig_art_id_t sub_4396A0(int64_t loc)
+{
+    int64_t sector_id;
+    Sector* sector;
+    tig_art_id_t aid;
+
+    sector_id = sub_4CFC50(loc);
+    if (!sector_lock(sector_id, &sector)) {
+        return TIG_ART_ID_INVALID;
+    }
+
+    aid = sector->roofs.field_4[sub_4395A0(loc)];
+
+    sector_unlock(sector_id);
+
+    return aid;
+}
+
+// 0x439700
+bool sub_439700(int64_t loc, tig_art_id_t aid)
+{
+    int64_t v1;
+    int64_t sector_id;
+    Sector* sector;
+    tig_art_id_t old_aid;
+    int64_t x;
+    int64_t y;
+    TigRect rect;
+
+    v1 = sub_4395E0(loc);
+    sector_id = sub_4CFC50(v1);
+    if (!sector_lock(sector_id, &sector)) {
+        return false;
+    }
+
+    old_aid = sector->roofs.field_4[sub_4395A0(v1)];
+    sector->roofs.field_4[sub_4395A0(v1)] = aid;
+    sector->roofs.field_0 = 0;
+
+    if (aid == TIG_ART_ID_INVALID) {
+        aid = old_aid;
+    }
+
+    sub_439640(v1, &x, &y);
+    if (x > INT_MIN
+        && x < INT_MAX
+        && y > INT_MIN
+        && y < INT_MAX) {
+        sub_43A140((int)x, (int)y, aid, &rect);
+        dword_5E2E34(&rect);
+    }
+
+    return true;
+}
+
+// 0x439890
+bool sub_439890(int x, int y)
+{
+    int64_t loc;
+    int64_t loc_x;
+    int64_t loc_y;
+    tig_art_id_t aid;
+    TigRect rect;
+
+    if (!dword_5E2E40) {
+        return false;
+    }
+
+    if (!sub_4B8730(x, y + 120, &loc)) {
+        return false;
+    }
+
+    aid = sub_4396A0(loc);
+    if (aid == TIG_ART_ID_INVALID) {
+        return false;
+    }
+
+    if (sub_5048D0(aid)) {
+        return false;
+    }
+
+    sub_439640(loc, &loc_x, &loc_y);
+    sub_43A140((int)loc_x, (int)loc_y, aid, &rect);
+    if (x >= rect.x
+        && y >= rect.y
+        && x < rect.x + rect.width
+        && y < rect.y + rect.height
+        && !sub_502FD0(aid, x - rect.x, y - rect.y)) {
+        return true;
+    }
+
+    return false;
+}
+
+// 0x439D30
+void sub_439D30(int64_t loc)
+{
+    tig_art_id_t aid;
+    int v1;
+    int tile;
+    int v5;
+    int v6;
+    bool v8;
+    bool v9;
+
+    aid = sub_4396A0(loc);
+    if (aid == TIG_ART_ID_INVALID) {
+        return;
+    }
+
+    v1 = sub_504840(aid);
+    tile = sub_4D7090((int)location_get_x(loc), (int)location_get_y(loc));
+    v5 = tile & 3;
+    v6 = (tile >> 6) % 4;
+
+    switch (v1) {
+    case 0:
+        v8 = v6 > 1 && v5 < 3;
+        break;
+    case 1:
+        v8 = v5 < 3;
+        break;
+    case 2:
+        v8 = v6 > 1;
+        break;
+    case 3:
+        if (v5 >= 3 && v6 <= 1) {
+            v8 = false;
+        } else {
+            v8 = true;
+        }
+        break;
+    case 4:
+        if (v5 >= 3) {
+            v8 = false;
+        } else {
+            if (v6 < 3) {
+                v8 = true;
+            } else {
+                v8 = false;
+            }
+        }
+        break;
+    case 5:
+        if (v5 >= 3) {
+            if (v6 < 3) {
+                v8 = true;
+            } else {
+                v8 = false;
+            }
+        } else {
+            v8 = true;
+        }
+        break;
+    case 6:
+        v8 = v5 > 1 || v6 > 1;
+        break;
+    case 7:
+        if (v5 <= 1 || v6 <= 1) {
+            v8 = false;
+        } else {
+            v8 = true;
+        }
+        break;
+    case 8:
+        v8 = true;
+        break;
+    case 9:
+        if (v5 <= 1) {
+            v8 = false;
+        } else {
+            if (v6 < 3) {
+                v8 = true;
+            } else {
+                v8 = false;
+            }
+        }
+        break;
+    case 10:
+        v8 = v6 < 3;
+        break;
+    case 11:
+        v8 = v5 > 1;
+        break;
+    case 12:
+        v8 = v5 > 1 || v6 < 3;
+        break;
+    default:
+        // Should be unreachable.
+        __assume(0);
+    }
+
+    v9 = sub_5048D0(aid);
+    if (v8 != v9) {
+        sub_43A1A0(sub_4395E0(loc), 1 - v9, -1);
+    }
+}
+
 // 0x439EA0
 void sub_439EA0(int64_t a1)
 {
@@ -128,34 +588,34 @@ void sub_439EA0(int64_t a1)
 }
 
 // 0x439EE0
-void sub_439EE0(int64_t a1)
+void sub_439EE0(int64_t loc)
 {
     tig_art_id_t aid;
 
-    aid = sub_4396A0(a1);
+    aid = sub_4396A0(loc);
     if (sub_504940(aid) == 0) {
-        sub_439700(a1, sub_504970(aid, 1));
+        sub_439700(loc, sub_504970(aid, 1));
     }
 }
 
 // 0x439F20
-void sub_439F20(int64_t a1)
+void sub_439F20(int64_t loc)
 {
     tig_art_id_t aid;
 
-    aid = sub_4396A0(a1);
+    aid = sub_4396A0(loc);
     if (sub_504940(aid) != 0) {
-        sub_439700(a1, sub_504970(aid, 0));
+        sub_439700(loc, sub_504970(aid, 0));
     }
 }
 
 // 0x439FA0
-bool sub_439FA0(int64_t a1)
+bool sub_439FA0(int64_t loc)
 {
     tig_art_id_t aid;
 
     if (dword_5E2E40) {
-        aid = sub_4396A0(a1);
+        aid = sub_4396A0(loc);
         if (aid != TIG_ART_ID_INVALID
             && sub_5048D0(aid) == 0
             && sub_504940(aid) != 0) {
@@ -175,19 +635,57 @@ bool sub_439FF0(int64_t a1, int64_t a2, int a3)
     return sub_43A030(v1, a3);
 }
 
-// 0x43A110
-void sub_43A110(int a1)
+// 0x43A030
+bool sub_43A030(int64_t loc, int a2)
 {
-    dword_5A53A0 = a1;
+    tig_art_id_t aid;
+
+    if (!dword_5E2E40) {
+        return false;
+    }
+
+    aid = sub_4D70B0(loc);
+    if (tig_art_tile_id_type_get(aid) != 0) {
+        return false;
+    }
+
+    aid = sub_4396A0(location_make(location_get_x(loc) + 3, location_get_y(loc) + 3));
+    if (aid == TIG_ART_ID_INVALID) {
+        return false;
+    }
+
+    if (sub_5048D0(aid)) {
+        return false;
+    }
+
+    if (!byte_5A53A4[sub_504840(aid)][(location_get_y(loc) + 3) % 4][(location_get_x(loc) + 3) % 4]) {
+        return false;
+    }
+
+    if (a2) {
+        return true;
+    }
+
+    if (sub_504940(aid)) {
+        return false;
+    }
+
+    return true;
+}
+
+// 0x43A110
+void roof_blit_flags_set(unsigned int flags)
+{
+    roof_blit_flags = flags;
     if (dword_5E2E34 != NULL) {
         dword_5E2E34(0);
     }
 }
 
 // 0x43A130
-int sub_43A130()
+unsigned int roof_blit_flags_get()
 {
-    return dword_5A53A0;
+    return roof_blit_flags;
 }
 
 // 0x43A140
@@ -205,5 +703,65 @@ void sub_43A140(int x, int y, tig_art_id_t aid, TigRect* rect)
         rect->y = y - art_frame_data.hot_y;
         rect->width = art_frame_data.width;
         rect->height = art_frame_data.height;
+    }
+}
+
+// 0x43A1A0
+void sub_43A1A0(int64_t loc, int a2, int a3)
+{
+    tig_art_id_t aid;
+    int v1;
+    int v2;
+
+    aid = sub_4396A0(loc);
+    while (aid != TIG_ART_ID_INVALID && sub_5048D0(aid) != a2) {
+        if (a3 != -1) {
+            v1 = sub_504840(aid);
+            switch (a3) {
+            case 1:
+                switch (v1) {
+                case 0:
+                case 1:
+                case 4:
+                    return;
+                };
+                break;
+            case 3:
+                switch (v1) {
+                case 0:
+                case 2:
+                case 7:
+                    return;
+                }
+                break;
+            case 5:
+                switch (v1) {
+                case 9:
+                case 7:
+                case 11:
+                    return;
+                }
+                break;
+            default:
+                if (a3 == 7) {
+                    switch (v1) {
+                    case 4:
+                    case 9:
+                    case 10:
+                        return;
+                    }
+                }
+                break;
+            }
+        }
+
+        v2 = sub_504900(aid, a2);
+        sub_439700(loc, v2);
+        sub_43A1A0(location_make(location_get_x(loc) + 4, location_get_y(loc) + 4), a2, 5);
+        sub_43A1A0(location_make(location_get_x(loc) - 4, location_get_y(loc) - 4), a2, 1);
+        sub_43A1A0(location_make(location_get_x(loc) + 4, location_get_y(loc) + 4), a2, 3);
+
+        a3 = 7;
+        aid = sub_4396A0(location_make(location_get_x(loc) - 4, location_get_y(loc) - 4));
     }
 }
--- src/game/roof.h
+++ src/game/roof.h
@@ -7,7 +7,17 @@ bool roof_init(GameInitInfo* init_info);
 void roof_exit();
 void roof_resize(ResizeInfo* resize_info);
 bool sub_439100(ViewOptions* view_options);
-void sub_43A110(int a1);
-int sub_43A130();
+void sub_439140(UnknownContext* info);
+int64_t sub_4395E0(int64_t a1);
+bool sub_439890(int x, int y);
+void sub_439D30(int64_t loc);
+void sub_439EA0(int64_t a1);
+void sub_439EE0(int64_t loc);
+void sub_439F20(int64_t loc);
+bool sub_439FA0(int64_t loc);
+bool sub_439FF0(int64_t a1, int64_t a2, int a3);
+bool sub_43A030(int64_t loc, int a2);
+void roof_blit_flags_set(unsigned int flags);
+unsigned int roof_blit_flags_get();
 
 #endif /* ARCANUM_GAME_ROOF_H_ */
