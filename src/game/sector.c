9b3b13353d5d2fed4b199ade38d345fdd671e094
Alexander Batalov <alex.batalov@gmail.com>
2024-11-24 10:04:35 +0300

    Fix sub_4D0090

--- src/game/sector.c
+++ src/game/sector.c
@@ -660,8 +660,8 @@ bool sub_4D0090(LocRect* rect, SomeSectorStuff* a2)
 
         for (x = 0; x < width; x++) {
             a2->field_8[y].field_8[x] = horizontal[x] | vertical[y];
-            a2->field_8[y].field_20[x] = sub_4CFC50(a2->field_8[y].field_8);
-            a2->field_8[y].field_38[x] = sub_4D7090(a2->field_8[y].field_8);
+            a2->field_8[y].field_20[x] = sub_4CFC50(a2->field_8[y].field_8[x]);
+            a2->field_8[y].field_38[x] = sub_4D7090(a2->field_8[y].field_8[x]);
             a2->field_8[y].field_44[x] = horizontal[1] - horizontal[0];
         }
 
