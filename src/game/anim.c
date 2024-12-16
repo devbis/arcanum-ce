0d6d8b374b83908ca5f4007565e54500606a18b0
Alexander Batalov <alex.batalov@gmail.com>
2024-12-16 23:21:11 +0300

    Add sub_431340

--- src/game/anim.c
+++ src/game/anim.c
@@ -247,6 +247,7 @@ static bool sub_4311F0(AnimRunInfo* run_info);
 static bool sub_431290(AnimRunInfo* run_info);
 static bool sub_431320(AnimRunInfo* run_info);
 static bool sub_431340(AnimRunInfo* run_info);
+static void sub_431550(AnimRunInfo* run_info, int64_t obj);
 static bool sub_4315B0(AnimRunInfo* run_info);
 static void sub_4319F0(AnimRunInfo* run_info, int64_t obj);
 static bool sub_431A40(AnimRunInfo* run_info);
@@ -11478,7 +11479,104 @@ bool sub_431320(AnimRunInfo* run_info)
 // 0x431340
 bool sub_431340(AnimRunInfo* run_info)
 {
-    // TODO: Incomplete.
+    int64_t obj;
+    int overlay_fore;
+    int overlay_back;
+    int overlay_light;
+    tig_art_id_t art_id;
+    TigArtAnimData art_anim_data;
+    int frame;
+
+    obj = run_info->params[0].obj;
+
+    ASSERT(obj != OBJ_HANDLE_NULL); // 13035, "obj != OBJ_HANDLE_NULL"
+
+    if (obj == OBJ_HANDLE_NULL) {
+        return false;
+    }
+
+    overlay_fore = run_info->cur_stack_data->params[AGDATA_SCRATCH_VAL1].data;
+    overlay_back = run_info->cur_stack_data->params[AGDATA_SCRATCH_VAL2].data;
+    overlay_light = run_info->cur_stack_data->params[AGDATA_SCRATCH_VAL3].data;
+
+    if (overlay_fore != -1 || overlay_back != -1) {
+        if (overlay_back != -5) {
+            if (overlay_fore != -1) {
+                art_id = sub_407470(obj, OBJ_F_OVERLAY_FORE, overlay_fore);
+                if (art_id == TIG_ART_ID_INVALID) {
+                    tig_debug_printf("Anim: AGupdateAnimEyeCandy: Error: No Art!\n");
+                    return false;
+                }
+
+                if (tig_art_anim_data(art_id, &art_anim_data) != TIG_OK) {
+                    return true;
+                }
+
+                frame = tig_art_id_frame_get(art_id);
+                if (frame == art_anim_data.num_frames - 1) {
+                    return false;
+                }
+
+                sub_43F9F0(obj, OBJ_F_OVERLAY_FORE, overlay_fore);
+
+                if (frame == art_anim_data.action_frame - 1) {
+                    run_info->field_C |= 0x04;
+                }
+            }
+
+            if (overlay_back != -1) {
+                art_id = sub_407470(obj, OBJ_F_OVERLAY_BACK, overlay_back);
+                if (art_id == TIG_ART_ID_INVALID) {
+                    tig_debug_printf("Anim: AGupdateAnimEyeCandy: Error: No Art!\n");
+                    return false;
+                }
+
+                if (tig_art_anim_data(art_id, &art_anim_data) != TIG_OK) {
+                    return true;
+                }
+
+                frame = tig_art_id_frame_get(art_id);
+                if (frame == art_anim_data.num_frames - 1) {
+                    return false;
+                }
+
+                sub_43F9F0(obj, OBJ_F_OVERLAY_BACK, overlay_back);
+            }
+        } else {
+            art_id = sub_407470(obj, OBJ_F_UNDERLAY, overlay_fore);
+            if (art_id == TIG_ART_ID_INVALID) {
+                tig_debug_printf("Anim: AGupdateAnimEyeCandy: Error: No Art!\n");
+                return false;
+            }
+
+            if (tig_art_anim_data(art_id, &art_anim_data) != TIG_OK) {
+                return true;
+            }
+
+            frame = tig_art_id_frame_get(art_id);
+                if (frame == art_anim_data.num_frames - 1) {
+                    return false;
+                }
+
+                sub_43F9F0(obj, OBJ_F_UNDERLAY, overlay_fore);
+
+                if (frame == art_anim_data.action_frame - 1) {
+                    run_info->field_C |= 0x04;
+                }
+        }
+    } else {
+        if (overlay_light != -1) {
+            return false;
+        }
+    }
+
+    if (overlay_light != -1) {
+        sub_43FC80(obj, overlay_light);
+    }
+
+    sub_431550(run_info, obj);
+
+    return true;
 }
 
 // 0x431550
