658cfdd87810b91a55f88b4ca5e211f67350b96c
Alexander Batalov <alex.batalov@gmail.com>
2025-01-03 08:52:25 +0300

    Clarify tile script art ids

--- src/game/tile_script.c
+++ src/game/tile_script.c
@@ -17,7 +17,7 @@ static bool sub_4C0930(int64_t loc);
 static void sub_4C0A40(int64_t sector_id, int tile, TigRect* rect);
 
 // 0x5FC868
-static tig_art_id_t dword_5FC868;
+static tig_art_id_t tile_script_iso_art_id;
 
 // 0x5FC86C
 static bool tile_script_editor;
@@ -38,7 +38,7 @@ static tig_window_handle_t tile_script_iso_window_handle;
 static ViewOptions tile_script_view_options;
 
 // 0x5FC888
-static tig_art_id_t dword_5FC888;
+static tig_art_id_t tile_script_td_art_id;
 
 // 0x4C0530
 bool tile_script_init(GameInitInfo* init_info)
@@ -48,8 +48,8 @@ bool tile_script_init(GameInitInfo* init_info)
     tile_script_editor = init_info->editor;
     tile_script_view_options.type = VIEW_TYPE_ISOMETRIC;
 
-    tig_art_interface_id_create(475, 0, 0, 0, &dword_5FC868);
-    tig_art_interface_id_create(476, 0, 0, 0, &dword_5FC888);
+    tig_art_interface_id_create(475, 0, 0, 0, &tile_script_iso_art_id);
+    tig_art_interface_id_create(476, 0, 0, 0, &tile_script_td_art_id);
 
     tile_script_initialized = true;
     dword_5FC878 = true;
@@ -111,10 +111,10 @@ void tile_script_render(UnknownContext* render_info)
 
     switch (tile_script_view_options.type) {
     case VIEW_TYPE_ISOMETRIC:
-        art_blit_info.art_id = dword_5FC868;
+        art_blit_info.art_id = tile_script_iso_art_id;
         break;
     case VIEW_TYPE_TOP_DOWN:
-        art_blit_info.art_id = dword_5FC888;
+        art_blit_info.art_id = tile_script_td_art_id;
         tig_art_frame_data(art_blit_info.art_id, &art_frame_data);
         src_rect.x = 0;
         src_rect.y = 0;
@@ -286,7 +286,7 @@ void sub_4C0A40(int64_t sector_id, int tile, TigRect* rect)
         return;
     }
 
-    if (tig_art_frame_data(dword_5FC868, &art_frame_data) != TIG_OK) {
+    if (tig_art_frame_data(tile_script_iso_art_id, &art_frame_data) != TIG_OK) {
         rect->x = 0;
         rect->y = 0;
         rect->width = 0;
