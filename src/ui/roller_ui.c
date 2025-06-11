d0a67d2d1b035c72e4dd42c9941857aa8e9b556d
Alexander Batalov <alex.batalov@gmail.com>
2025-06-11 20:15:32 +0300

    Review roller ui

--- src/ui/roller_ui.c
+++ src/ui/roller_ui.c
@@ -3,25 +3,25 @@
 #include <stdio.h>
 
 // 0x67BB18
-static tig_font_handle_t dword_67BB18;
+static tig_font_handle_t roller_ui_red_font;
 
 // 0x67BB1C
-static int dword_67BB1C;
+static int roller_ui_sign_width;
 
 // 0x67BB20
-static tig_font_handle_t dword_67BB20;
+static tig_font_handle_t roller_ui_black_font;
 
 // 0x67BB24
 static TigVideoBuffer* roller_ui_video_buffer;
 
 // 0x67BB28
-static tig_font_handle_t dword_67BB28;
+static tig_font_handle_t roller_ui_green_font;
 
 // 0x67BB2C
-static int dword_67BB2C;
+static int roller_ui_digit_width;
 
 // 0x67BB30
-static int dword_67BB30;
+static int roller_ui_digit_height;
 
 // 0x67BB34
 static bool roller_ui_initialized;
@@ -41,38 +41,38 @@ bool roller_ui_init(GameInitInfo* init_info)
     font_desc.str = NULL;
 
     font_desc.color = tig_color_make(0, 0, 0);
-    tig_font_create(&font_desc, &dword_67BB20);
+    tig_font_create(&font_desc, &roller_ui_black_font);
 
     font_desc.color = tig_color_make(0, 110, 0);
-    tig_font_create(&font_desc, &dword_67BB28);
+    tig_font_create(&font_desc, &roller_ui_green_font);
 
     font_desc.color = tig_color_make(192, 32, 32);
-    tig_font_create(&font_desc, &dword_67BB18);
+    tig_font_create(&font_desc, &roller_ui_red_font);
 
-    tig_font_push(dword_67BB20);
+    tig_font_push(roller_ui_black_font);
 
     font_desc.str = "+";
     font_desc.width = 0;
     tig_font_measure(&font_desc);
-    dword_67BB1C = font_desc.width;
+    roller_ui_sign_width = font_desc.width;
 
     font_desc.str = "0";
     font_desc.width = 0;
     tig_font_measure(&font_desc);
-    dword_67BB2C = font_desc.width;
-    dword_67BB30 = font_desc.height;
+    roller_ui_digit_width = font_desc.width;
+    roller_ui_digit_height = font_desc.height;
 
     tig_font_pop();
 
     vb_create_info.flags = TIG_VIDEO_BUFFER_VIDEO_MEMORY;
-    vb_create_info.width = dword_67BB1C + dword_67BB2C;
-    vb_create_info.height = dword_67BB30;
+    vb_create_info.width = roller_ui_sign_width + roller_ui_digit_width;
+    vb_create_info.height = roller_ui_digit_height;
     vb_create_info.background_color = 0;
 
     if (tig_video_buffer_create(&vb_create_info, &roller_ui_video_buffer) != TIG_OK) {
-        tig_font_destroy(dword_67BB20);
-        tig_font_destroy(dword_67BB28);
-        tig_font_destroy(dword_67BB18);
+        tig_font_destroy(roller_ui_black_font);
+        tig_font_destroy(roller_ui_green_font);
+        tig_font_destroy(roller_ui_red_font);
         return false;
     }
 
@@ -88,9 +88,9 @@ bool roller_ui_init(GameInitInfo* init_info)
     blit_info.dst_video_buffer = roller_ui_video_buffer;
 
     if (tig_art_blit(&blit_info) != TIG_OK) {
-        tig_font_destroy(dword_67BB20);
-        tig_font_destroy(dword_67BB28);
-        tig_font_destroy(dword_67BB18);
+        tig_font_destroy(roller_ui_black_font);
+        tig_font_destroy(roller_ui_green_font);
+        tig_font_destroy(roller_ui_red_font);
         tig_video_buffer_destroy(roller_ui_video_buffer);
         return false;
     }
@@ -107,16 +107,16 @@ void roller_ui_exit()
         return;
     }
 
-    tig_font_destroy(dword_67BB20);
-    tig_font_destroy(dword_67BB28);
-    tig_font_destroy(dword_67BB18);
+    tig_font_destroy(roller_ui_black_font);
+    tig_font_destroy(roller_ui_green_font);
+    tig_font_destroy(roller_ui_red_font);
     tig_video_buffer_destroy(roller_ui_video_buffer);
 
     roller_ui_initialized = false;
 }
 
 // 0x56A300
-void roller_ui_draw(int value, tig_window_handle_t window_handle, int x, int y, int max_digits, int flags)
+void roller_ui_draw(int value, tig_window_handle_t window_handle, int x, int y, int num_digits, RollerFlags flags)
 {
     const char* sign;
     TigRect dst_rect;
@@ -124,26 +124,26 @@ void roller_ui_draw(int value, tig_window_handle_t window_handle, int x, int y, 
     int dig;
     char buffer[2];
 
-    if ((flags & 0x4) != 0) {
-        tig_font_push(dword_67BB28);
-    } else if ((flags & 0x8) != 0) {
-        tig_font_push(dword_67BB18);
+    if ((flags & ROLLER_GREEN) != 0) {
+        tig_font_push(roller_ui_green_font);
+    } else if ((flags & ROLLER_RED) != 0) {
+        tig_font_push(roller_ui_red_font);
     } else {
-        tig_font_push(dword_67BB20);
+        tig_font_push(roller_ui_black_font);
     }
 
     dst_rect.x = x;
     dst_rect.y = y;
 
-    if ((flags & 0x1) != 0) {
+    if ((flags & ROLLER_SIGN) != 0) {
         sign = value >= 0 ? "+" : "-";
-        dst_rect.width = dword_67BB1C;
-        dst_rect.height = dword_67BB30;
+        dst_rect.width = roller_ui_sign_width;
+        dst_rect.height = roller_ui_digit_height;
 
         src_rect.x = 0;
         src_rect.y = 0;
-        src_rect.width = dword_67BB1C;
-        src_rect.height = dword_67BB30;
+        src_rect.width = roller_ui_sign_width;
+        src_rect.height = roller_ui_digit_height;
 
         tig_window_copy_from_vbuffer(window_handle,
             &dst_rect,
@@ -151,28 +151,28 @@ void roller_ui_draw(int value, tig_window_handle_t window_handle, int x, int y, 
             &src_rect);
         tig_window_text_write(window_handle, sign, &dst_rect);
 
-        dst_rect.x += dword_67BB1C;
+        dst_rect.x += roller_ui_sign_width;
     }
 
-    dst_rect.x += dword_67BB2C * (max_digits - 1);
-    dst_rect.width = dword_67BB2C;
-    dst_rect.height = dword_67BB30;
+    dst_rect.x += roller_ui_digit_width * (num_digits - 1);
+    dst_rect.width = roller_ui_digit_width;
+    dst_rect.height = roller_ui_digit_height;
 
-    src_rect.x = dword_67BB1C;
+    src_rect.x = roller_ui_sign_width;
     src_rect.y = 0;
-    src_rect.width = dword_67BB2C;
-    src_rect.height = dword_67BB30;
+    src_rect.width = roller_ui_digit_width;
+    src_rect.height = roller_ui_digit_height;
 
-    for (dig = 0; dig < max_digits; dig++) {
+    for (dig = 0; dig < num_digits; dig++) {
         snprintf(buffer, sizeof(buffer), "%d", value % 10);
         tig_window_copy_from_vbuffer(window_handle,
             &dst_rect,
             roller_ui_video_buffer,
             &src_rect);
-        if (dig == 0 || value != 0 || (flags & 0x2) != 0) {
+        if (dig == 0 || value != 0 || (flags & ROLLER_ZERO) != 0) {
             tig_window_text_write(window_handle, buffer, &dst_rect);
         }
-        dst_rect.x -= dword_67BB2C;
+        dst_rect.x -= roller_ui_digit_width;
         value /= 10;
     }
 
--- src/ui/roller_ui.h
+++ src/ui/roller_ui.h
@@ -3,8 +3,15 @@
 
 #include "game/context.h"
 
+typedef unsigned int RollerFlags;
+
+#define ROLLER_SIGN 0x01u
+#define ROLLER_ZERO 0x02u
+#define ROLLER_GREEN 0x04u
+#define ROLLER_RED 0x08u
+
 bool roller_ui_init(GameInitInfo* init_info);
 void roller_ui_exit();
-void roller_ui_draw(int value, tig_window_handle_t window_handle, int x, int y, int max_digits, int flags);
+void roller_ui_draw(int value, tig_window_handle_t window_handle, int x, int y, int num_digits, RollerFlags flags);
 
 #endif /* ARCANUM_UI_ROLLER_UI_H_ */
