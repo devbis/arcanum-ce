4cf91aeed04ec8da6c3659ca1ca5b7dda33925f3
Alexander Batalov <alex.batalov@gmail.com>
2025-06-19 23:13:42 +0300

    Add sector script docs

--- src/game/sector_script.c
+++ src/game/sector_script.c
@@ -2,7 +2,11 @@
 
 #include "game/sector.h"
 
-// 0x4C0410
+/**
+ * Called when the game is initialized.
+ *
+ * 0x4C0410
+ */
 bool sector_script_init(GameInitInfo* init_info)
 {
     (void)init_info;
@@ -10,23 +14,34 @@ bool sector_script_init(GameInitInfo* init_info)
     return true;
 }
 
-// 0x4C0420
+/**
+ * Called when the game is being reset.
+ *
+ * 0x4C0420
+ */
 void sector_script_reset()
 {
 }
 
-// 0x4C0430
+/**
+ * Called when the game shuts down.
+ *
+ * 0x4C0430
+ */
 void sector_script_exit()
 {
 }
 
-// 0x4C0440
+/**
+ * Retrieves the script associated with a sector.
+ *
+ * 0x4C0440
+ */
 bool sector_script_get(int64_t sector_id, Script* scr)
 {
     Sector* sector;
-    bool ret;
+    bool ret = false;
 
-    ret = false;
     if (sector_lock(sector_id, &sector)) {
         ret = sector_script_list_get(&(sector->sector_scripts), scr);
         sector_unlock(sector_id);
@@ -35,13 +50,16 @@ bool sector_script_get(int64_t sector_id, Script* scr)
     return ret;
 }
 
-// 0x4C0490
+/**
+ * Sets the script for a sector.
+ *
+ * 0x4C0490
+ */
 bool sector_script_set(int64_t sector_id, Script* scr)
 {
     Sector* sector;
-    bool ret;
+    bool ret = false;
 
-    ret = false;
     if (sector_lock(sector_id, &sector)) {
         ret = sector_script_list_set(&(sector->sector_scripts), scr);
         sector_unlock(sector_id);
