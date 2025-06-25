426bfc1362ece197b879b3ac8c0ecb1aff6d7c96
Alexander Batalov <alex.batalov@gmail.com>
2025-06-25 07:28:32 +0300

    Add tech ui docs

--- src/ui/tech_ui.c
+++ src/ui/tech_ui.c
@@ -7,7 +7,11 @@
 #include "ui/charedit_ui.h"
 #include "ui/intgame.h"
 
-// 0x589A20
+/**
+ * Increases the degree of a specified technology for a critter.
+ *
+ * 0x589A20
+ */
 void tech_ui_inc_degree(int64_t obj, int tech)
 {
     int degree;
@@ -21,23 +25,34 @@ void tech_ui_inc_degree(int64_t obj, int tech)
         return;
     }
 
+    // Retrieve current degree, cost for next degree, and available points.
     degree = tech_degree_get(obj, tech);
     cost = tech_degree_cost_get(degree + 1);
     points = stat_level_get(obj, STAT_UNSPENT_POINTS);
     is_pc = player_is_local_pc_obj(obj);
 
+    // Check if there are enough character points to increment the degree.
     if (cost > points) {
+        // Notify player of insufficient points.
         if (is_pc) {
             charedit_error_not_enough_character_points();
         }
     } else {
+        // Attempt to increase the technology degree.
         if (tech_degree_inc(obj, tech) > degree) {
+            // Deduct cost from unspent points.
             stat_base_set(obj, STAT_UNSPENT_POINTS, points - cost);
+
+            // Update UI.
             if (is_pc) {
                 charedit_refresh();
                 sub_550720();
             }
         } else {
+            // Notify player of insufficient intelligence.
+            //
+            // NOTE: In fact there could be other reasons why next degree was
+            // not obtained.
             if (is_pc) {
                 charedit_error_not_enough_intelligence();
             }
@@ -45,7 +60,11 @@ void tech_ui_inc_degree(int64_t obj, int tech)
     }
 }
 
-// 0x589AF0
+/**
+ * Decreases the degree of a specified technology for a critter.
+ *
+ * 0x589AF0
+ */
 void tech_ui_dec_degree(int64_t obj, int tech)
 {
     int degree;
@@ -59,13 +78,19 @@ void tech_ui_dec_degree(int64_t obj, int tech)
         return;
     }
 
+    // Retrieve current degree, cost of the current degree, and available
+    // points.
     degree = tech_degree_get(obj, tech);
     cost = tech_degree_cost_get(degree);
     points = stat_level_get(obj, STAT_UNSPENT_POINTS);
     is_pc = player_is_local_pc_obj(obj);
 
+    // Attempt to decrease the technology degree.
     if (tech_degree_dec(obj, tech) < degree) {
+        // Refund cost of the degree to unspent points.
         stat_base_set(obj, STAT_UNSPENT_POINTS, points + cost);
+
+        // Update UI.
         if (is_pc) {
             charedit_refresh();
             sub_550720();
@@ -73,7 +98,12 @@ void tech_ui_dec_degree(int64_t obj, int tech)
     }
 }
 
-// 0x589B90
+/**
+ * Called when the multiplayer game receives `mpPacketTypePlayerRequestTechChange`
+ * packet.
+ *
+ * 0x589B90
+ */
 void tech_ui_adjust_degree(int64_t obj, int tech, int action)
 {
     switch (action) {
