83420df4a38b4bb477f02fa41a78b06b253dd548
Alexander Batalov <alex.batalov@gmail.com>
2025-07-13 00:01:45 +0300

    Add sector script list docs

--- src/game/sector_script_list.c
+++ src/game/sector_script_list.c
@@ -1,12 +1,20 @@
 #include "game/sector_script_list.h"
 
-// 0x4F6170
+/**
+ * Initializes a script list.
+ *
+ * 0x4F6170
+ */
 bool sector_script_list_init(SectorScriptList* list)
 {
     return sector_script_list_reset(list);
 }
 
-// 0x4F6180
+/**
+ * Resets a script list.
+ *
+ * 0x4F6180
+ */
 bool sector_script_list_reset(SectorScriptList* list)
 {
     list->flags = 0;
@@ -17,13 +25,21 @@ bool sector_script_list_reset(SectorScriptList* list)
     return true;
 }
 
-// 0x4F61B0
+/**
+ * Frees a script list.
+ *
+ * 0x4F61B0
+ */
 bool sector_script_list_exit(SectorScriptList* list)
 {
     return sector_script_list_reset(list);
 }
 
-// 0x4F61C0
+/**
+ * Sets the script to a script list.
+ *
+ * 0x4F61C0
+ */
 bool sector_script_list_set(SectorScriptList* list, Script* scr)
 {
     list->scr = *scr;
@@ -31,7 +47,11 @@ bool sector_script_list_set(SectorScriptList* list, Script* scr)
     return true;
 }
 
-// 0x4F61F0
+/**
+ * Retrieves the script from a script list.
+ *
+ * 0x4F61F0
+ */
 bool sector_script_list_get(SectorScriptList* list, Script* scr)
 {
     *scr = list->scr;
@@ -39,7 +59,11 @@ bool sector_script_list_get(SectorScriptList* list, Script* scr)
     return scr->num != 0;
 }
 
-// 0x4F6220
+/**
+ * Clears script list.
+ *
+ * 0x4F6220
+ */
 bool sector_script_list_remove(SectorScriptList* list)
 {
     if (list->scr.num != 0) {
@@ -50,7 +74,11 @@ bool sector_script_list_remove(SectorScriptList* list)
     return true;
 }
 
-// 0x4F6250
+/**
+ * Loads a script list from a file.
+ *
+ * 0x4F6250
+ */
 bool sector_script_list_load(SectorScriptList* list, TigFile* stream)
 {
     if (tig_file_fread(&(list->scr), sizeof(list->scr), 1, stream) != 1) {
@@ -60,7 +88,11 @@ bool sector_script_list_load(SectorScriptList* list, TigFile* stream)
     return true;
 }
 
-// 0x4F6270
+/**
+ * Saves a script list to a file.
+ *
+ * 0x4F6270
+ */
 bool sector_script_list_save(SectorScriptList* list, TigFile* stream)
 {
     if (tig_file_fwrite(&(list->scr), sizeof(list->scr), 1, stream) != 1) {
@@ -72,13 +104,21 @@ bool sector_script_list_save(SectorScriptList* list, TigFile* stream)
     return true;
 }
 
-// 0x4F62A0
+/**
+ * Checks if the script list has been modified.
+ *
+ * 0x4F62A0
+ */
 bool sector_script_list_is_modified(SectorScriptList* list)
 {
     return (list->flags & SECTOR_SCRIPT_LIST_MODIFIED) != 0;
 }
 
-// 0x4F62B0
+/**
+ * Loads updates for a script list from a dif file.
+ *
+ * 0x4F62B0
+ */
 bool sector_script_list_load_with_dif(SectorScriptList* list, TigFile* stream)
 {
     if (!sector_script_list_load(list, stream)) {
@@ -90,13 +130,19 @@ bool sector_script_list_load_with_dif(SectorScriptList* list, TigFile* stream)
     return true;
 }
 
-// 0x4F62E0
+/**
+ * Saves updates for a script list to a dif file.
+ *
+ * 0x4F62E0
+ */
 bool sector_script_list_save_with_dif(SectorScriptList* list, TigFile* stream)
 {
     if (!sector_script_list_save(list, stream)) {
         return false;
     }
 
+    // FIXME: This looks wrong. This flag should be unset after script list is
+    // saved.
     list->flags |= SECTOR_SCRIPT_LIST_MODIFIED;
 
     return true;
