e559bd3a296c65d29c21526a2751f861e9aa69cb
Alexander Batalov <alex.batalov@gmail.com>
2025-07-15 07:25:57 +0300

    Add gfade docs

--- src/game/gfade.c
+++ src/game/gfade.c
@@ -2,13 +2,25 @@
 
 #include "game/li.h"
 
-// 0x5FC4B0
+/**
+ * Function pointer to invalidate a rectangle in the parent window.
+ *
+ * 0x5FC4B0
+ */
 static IsoInvalidateRectFunc* gfade_iso_invalidate_rect;
 
-// 0x5FC4BC
+/**
+ * Function pointer to flush dirty iso content onto window.
+ *
+ * 0x5FC4BC
+ */
 static IsoRedrawFunc* gfade_iso_draw;
 
-// 0x4BDE70
+/**
+ * Called when the game is initialized.
+ *
+ * 0x4BDE70
+ */
 bool gfade_init(GameInitInfo* init_info)
 {
     gfade_iso_invalidate_rect = init_info->invalidate_rect_func;
@@ -17,23 +29,43 @@ bool gfade_init(GameInitInfo* init_info)
     return true;
 }
 
-// 0x4BDF60
+/**
+ * Called when the game shuts down.
+ *
+ * 0x4BDF60
+ */
 void gfade_exit()
 {
 }
 
-// 0x4BDF80
+/**
+ * Called when the window size has changed.
+ *
+ * 0x4BDF80
+ */
 void gfade_resize(GameResizeInfo* resize_info)
 {
+    (void)resize_info;
 }
 
-// 0x4BDFA0
+/**
+ * Executes a fade effect based on the provided fade data.
+ *
+ * 0x4BDFA0
+ */
 void gfade_run(FadeData* fade_data)
 {
-    tig_video_fade(fade_data->color, fade_data->steps, fade_data->duration, fade_data->flags);
+    tig_video_fade(fade_data->color,
+        fade_data->steps,
+        fade_data->duration,
+        fade_data->flags);
 }
 
-// 0x4BE050
+/**
+ * Called when a fade timeevent occurs.
+ *
+ * 0x4BE050
+ */
 bool gfade_timeevent_process(TimeEvent* timeevent)
 {
     FadeData fade_data;
@@ -43,6 +75,8 @@ bool gfade_timeevent_process(TimeEvent* timeevent)
     fade_data.duration = timeevent->params[2].float_value;
     fade_data.steps = timeevent->params[3].integer_value;
 
+    // Before fading in, update iso content while window is still faded. Once
+    // the fade is removed, the world will appear updated.
     if ((fade_data.flags & FADE_IN) != 0) {
         gfade_iso_invalidate_rect(NULL);
         gfade_iso_draw();
@@ -50,6 +84,7 @@ bool gfade_timeevent_process(TimeEvent* timeevent)
         sub_4BBC00();
     }
 
+    // Execute the fade effect.
     gfade_run(&fade_data);
 
     return true;
